1ae46323e5b81fa07553c71ab04866e370827156
Jumpei Ogawa <git@phanective.org>
2024-05-15 08:14:37 +0900

    style: fix coding styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	import '../app.postcss';
-	import Nav from '$lib/components/Nav.svelte'
-	import type {LayoutData} from './$types';
-	export let data: LayoutData;
+  import "@skeletonlabs/skeleton/themes/theme-gold-nouveau.css";
+  import "@skeletonlabs/skeleton/styles/skeleton.css";
+  import "../app.postcss";
+  import Nav from "$lib/components/Nav.svelte";
+  import type { LayoutData } from "./$types";
+  export let data: LayoutData;
 </script>
 
-<Nav {data}/>
+<Nav {data} />
 <main class="max-h-fit">
-	<slot />
+  <slot />
 </main>
--- src/routes/auth/logout/+page.server.ts
+++ src/routes/auth/logout/+page.server.ts
@@ -1,13 +1,13 @@
-import type {Actions} from "@sveltejs/kit";
-import {redirect} from "@sveltejs/kit";
+import type { Actions } from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit";
 
 export const actions = {
-    default: async ({cookies})=>{
-        cookies.set("token", "", {
-            httpOnly: true,
-            path: '/',
-            maxAge: 0
-        });
-        throw redirect(303, '/auth/login');
-    }
+  default: async ({ cookies }) => {
+    cookies.set("token", "", {
+      httpOnly: true,
+      path: "/",
+      maxAge: 0,
+    });
+    throw redirect(303, "/auth/login");
+  },
 } satisfies Actions;
