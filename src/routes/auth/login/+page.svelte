b7d0d05da6ca5ae505325a62b362b9537c2bb33e
Jumpei Ogawa <git@phanective.org>
2024-05-16 05:00:41 +0900

    fix: replace possibly existing email domain with `@example.com` as a placeholder

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -60,7 +60,7 @@
         <input
           class={$errors.email ? "input-error block w-full" : "input"}
           type="email"
-          placeholder="johndoe@email.com"
+          placeholder="johndoe@example.com"
           name="email"
           id="email"
           bind:value={$form.email}
