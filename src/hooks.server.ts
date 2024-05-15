72838207e658645bafe9216e43b069a25ff2bc0a
Jumpei Ogawa <git@phanective.org>
2024-05-16 05:03:09 +0900

    wip: consider better solution for this type check error

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,18 @@
 import type { Handle } from "@sveltejs/kit";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "$env/static/private";
-import { redirect } from "@sveltejs/kit";
+import { error, redirect } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
   const token = event.cookies.get("token");
   const path = event.route.id;
 
-  if (path?.includes("protected") && !token) {
-    throw redirect(303, "/auth/login");
+  if (!token) {
+    if (path?.includes("protected")) {
+      throw redirect(303, "/auth/login");
+    } else {
+      throw error(500, "Could not get tokens"); // TODO
+    }
   }
   if (path?.includes("admin")) {
     try {
