23279fbfc00743cb96b502d6196722b3e5de82f3
Jumpei Ogawa <git@phanective.org>
2025-02-12 09:03:16 +0900

    feat: replace icons from unknown source

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -21,25 +21,7 @@
           class="h-16 px-6 flex justify-center items-center w-full
           focus:text-orange-500"
         >
-          <svg
-            class="h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <polyline points="22 12 16 12 14 15 10 15 8 12 2 12" />
-            <path
-              d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0
-              2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0
-              0-1.79 1.11z"
-            />
-          </svg>
+          <img src="/vendor/tabler/inbox.svg" alt="Inbox" decoding="async" />
         </a>
       </li>
 
@@ -49,20 +31,7 @@
           class="h-16 px-6 flex justify-center items-center w-full
           focus:text-orange-500"
         >
-          <svg
-            class="h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
-          </svg>
+          <img src="/vendor/tabler/bookmark.svg" alt="Bookmarks" decoding="async" />
         </a>
       </li>
 
@@ -72,24 +41,7 @@
           class="h-16 px-6 flex justify-center items-center w-full
           focus:text-orange-500"
         >
-          <svg
-            class="h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <polyline points="3 6 5 6 21 6" />
-            <path
-              d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2
-              0 0 1 2-2h4a2 2 0 0 1 2 2v2"
-            />
-          </svg>
+          <img src="/vendor/tabler/trash.svg" alt="Trash" decoding="async" />
         </a>
       </li>
 
@@ -99,25 +51,7 @@
           class="h-16 px-6 flex justify-center items-center w-full
           focus:text-orange-500"
         >
-          <svg
-            class="h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <circle cx="9" cy="21" r="1" />
-            <circle cx="20" cy="21" r="1" />
-            <path
-              d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0
-              2-1.61L23 6H6"
-            />
-          </svg>
+          <img src="/vendor/tabler/shopping-cart.svg" alt="Cart" decoding="async" />
         </a>
       </li>
 
@@ -127,37 +61,7 @@
           class="h-16 px-6 flex justify-center items-center w-full
           focus:text-orange-500"
         >
-          <svg
-            class="h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <circle cx="12" cy="12" r="3" />
-            <path
-              d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1
-              0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0
-              0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2
-              2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0
-              0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1
-              0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0
-              0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65
-              0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0
-              1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0
-              1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2
-              0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0
-              1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0
-              2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0
-              0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65
-              1.65 0 0 0-1.51 1z"
-            />
-          </svg>
+          <img src="/vendor/tabler/settings.svg" alt="Settings" decoding="async" />
         </a>
       </li>
 
@@ -167,21 +71,7 @@
           class="h-16 px-6 flex justify-center items-center w-full
           focus:text-orange-500"
         >
-          <svg
-            class="h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
-            <path d="M13.73 21a2 2 0 0 1-3.46 0" />
-          </svg>
+          <img src="/vendor/tabler/bell.svg" alt="Notifications" decoding="async" />
         </a>
       </li>
     </ul>
@@ -195,22 +85,7 @@
         class="focus:text-orange-500 hover:bg-red-200 focus:outline-none w-full"
       >
         <button class="h-16 flex justify-center items-center mx-auto" type="submit">
-          <svg
-            class="h-5 w-5 text-red-700"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-            <polyline points="16 17 21 12 16 7" />
-            <line x1="21" y1="12" x2="9" y2="12" />
-          </svg>
+          <img src="/vendor/tabler/logout-2.svg" alt="Logout" decoding="async" />
         </button>
       </form>
     </div>
---
+++ static/vendor/CREDITS.md
@@ -0,0 +1,10 @@
+# Thirdparty icon credits
+
+- [Octicons](https://primer.style/foundations/icons)
+  - licensed under MIT
+  - by GitHub, Inc.
+  - [repository](https://github.com/primer/octicons)
+- [Tabler Icons](https://tabler.io/icons)
+  - licensed under MIT
+  - by Paweł Kuna
+  - [repository](https://github.com/tabler/tabler-icons)
