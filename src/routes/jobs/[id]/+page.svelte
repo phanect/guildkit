f3839839f3345b6a9d89bc3c8734b65ecb64eb02
Jumpei Ogawa <git@phanective.org>
2025-02-12 09:03:16 +0900

    feat: replace icons from unknown source

--- src/routes/jobs/[id]/+page.svelte
+++ src/routes/jobs/[id]/+page.svelte
@@ -12,26 +12,26 @@
   <div class="border border-gray-200 min-h-screen rounded-lg p-12">
     <h1 class="text-3xl font-black text-gray-900">{job.title}</h1>
     <p class="italic inline-flex gap-2 mt-2 text-gray-400">
-      <img src="/assets/icons/at.svg" alt="At Symbol Icon" width="18" />
+      <img src="/vendor/octicons/mention.svg" alt="At Symbol Icon" width="18" />
       {job.company}
     </p>
     <div class="mt-4">
       <span
         class="bg-gray-200 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full inline-flex gap-2"
       >
-        <img src="/assets/icons/location.svg" alt="Location Icon" width="16" />
+        <img src="/vendor/octicons/location.svg" alt="Location Icon" width="16" />
         {job.location}
       </span>
       <span
         class="bg-green-200 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full inline-flex gap-2"
       >
-        <img src="/assets/icons/money.svg" alt="Money Icon" width="16" />
+        <img src="/vendor/zondicons/currency-dollar.svg" alt="Money Icon" width="16" />
         {job.salary}
       </span>
       <span
         class="bg-red-200 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full inline-flex gap-2"
       >
-        <img src="/assets/icons/clock.svg" alt="Money Icon" width="16" />
+        <img src="/vendor/octicons/clock.svg" alt="Money Icon" width="16" />
         {getRemainingTime(job.deadline)}
       </span>
     </div>
