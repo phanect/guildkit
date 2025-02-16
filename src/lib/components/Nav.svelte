de2420971561db7c3867fce145681a8cf54b822e
Jumpei Ogawa <git@phanective.org>
2025-02-16 19:42:06 +0900

    feat: delete icons from unknown source

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -10,17 +10,7 @@
 
 <nav class="flex items-center justify-between flex-wrap py-6 px-20">
   <div class="flex items-center flex-shrink-0 mr-6">
-    <svg
-      class="fill-current h-8 w-8 mr-2"
-      width="54"
-      height="54"
-      viewBox="0 0 54 54"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
-      />
-    </svg>
+    <img src="/images/to-be-added.svg" alt="GuildKit" decoding="async" />
     <a href="/" class="font-semibold text-xl tracking-tight">Job Finder</a>
   </div>
   <div class="flex items-center">
