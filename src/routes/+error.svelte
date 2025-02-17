c62f787b7b0fd5afcee6ef8e1e381a67f73bd2eb
Jumpei Ogawa <git@phanective.org>
2025-02-17 13:10:57 +0900

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { page } from "$app/state";
+</script>
+
+{#if page.status === 404}
+  <h1>This page is not found.</h1>
+{:else}
+  <h1>{ page.status }: { page.error?.message }</h1>
+{/if}
