11600950d3a1c682879d7166d332005cb3a7f7d1
Jumpei Ogawa <git@phanective.org>
2025-02-21 17:27:57 +0900

    refactor: prefer to add type to variables to using `satisfies`

--- src/routes/jobs/[id]/+page.server.ts
+++ src/routes/jobs/[id]/+page.server.ts
@@ -2,7 +2,7 @@ import { error } from "@sveltejs/kit";
 import prisma from "$lib/prisma.ts";
 import type { PageServerLoad } from "./$types";
 
-export const load = (async ({ params: { id }}) => {
+export const load: PageServerLoad = async ({ params: { id }}) => {
   const job = await prisma.job.findUnique({
     where: {
       id,
@@ -14,4 +14,4 @@ export const load = (async ({ params: { id }}) => {
   }
 
   return { job };
-}) satisfies PageServerLoad;
+};
