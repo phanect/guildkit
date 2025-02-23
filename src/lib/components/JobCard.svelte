10e4f7b5c80cd788e435a3091c48ceaeef40d9ce
Jumpei Ogawa <git@phanective.org>
2025-02-23 10:09:38 +0900

    refactor: reduce duplicate code from `JobCard`

--- src/lib/components/JobCard.svelte
+++ src/lib/components/JobCard.svelte
@@ -16,29 +16,30 @@
   };
 
   const { job, role }: Props = $props();
+  const editable = (role && (role == "EMPLOYER" || role === "ADMIN"));
 </script>
 
 <div class="overflow-hidden group relative rounded-lg p-[1px] flex justify-center items-center">
   <div
     class="rounded-lg hidden group-hover:block animate-rotate-color w-[250%] h-[500%] absolute -top-[60%] -left-[50%] bg-gradient-to-r from-zinc-900 via-gray-200/40 to-zinc-700 shadow-xl"
   ></div>
-  {#if role}
-    <a class="block w-full relative z-10 rounded-lg bg-white p-6 sm:p-8" href="jobs/{job.id}">
-      <div class="mt-12 sm:pr-8">
-        <h3 class="text-xl font-bold text-gray-900">
-          {job.title}
-        </h3>
-        <p class="mt-2 text-sm text-gray-500 truncate">
-          {job.description}
-        </p>
-        <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-        <div class="text-sm flex justify-between">
-          <p class="text-gray-700">Deadline</p>
-          <div class="flex flex-col items-end">
-            <p class="text-gray-400">{job.deadline.toLocaleDateString()}</p>
-            <p class="text-red-300">({getRemainingTime(job.deadline)})</p>
-          </div>
+  <a class="block w-full relative z-10 rounded-lg bg-white p-6 sm:p-8" href="jobs/{job.id}">
+    <div class="mt-12 sm:pr-8">
+      <h3 class="text-xl font-bold text-gray-900">
+        {job.title}
+      </h3>
+      <p class="mt-2 text-sm text-gray-500 truncate">
+        {job.description}
+      </p>
+      <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+      <div class="text-sm flex justify-between">
+        <p class="text-gray-700">Deadline</p>
+        <div class="flex flex-col items-end">
+          <p class="text-gray-400">{job.deadline.toLocaleDateString()}</p>
+          <p class="text-red-300">({getRemainingTime(job.deadline)})</p>
+        </div>
 
+        {#if editable}
           <div class="flex items-end gap-2">
             <button
               type="submit"
@@ -58,27 +59,8 @@
               </button>
             </form>
           </div>
-        </div>
+        {/if}
       </div>
-    </a>
-  {:else}
-    <a class="block w-full relative z-10 rounded-lg bg-white p-6 sm:p-8" href="jobs/{job.id}">
-      <div class="mt-12 sm:pr-8">
-        <h3 class="text-xl font-bold text-gray-900">
-          {job.title}
-        </h3>
-        <p class="mt-2 text-sm text-gray-500 truncate">
-          {job.description}
-        </p>
-        <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-        <div class="text-sm flex justify-between">
-          <p class="text-gray-700">Deadline</p>
-          <div class="flex flex-col items-end">
-            <p class="text-gray-400">{job.deadline.toLocaleDateString()}</p>
-            <p class="text-red-300">({getRemainingTime(job.deadline)})</p>
-          </div>
-        </div>
-      </div>
-    </a>
-  {/if}
+    </div>
+  </a>
 </div>
