b88790c9b03e0509fb47bba375e90d4534a3a667
Jumpei Ogawa <git@phanective.org>
2025-03-22 11:38:30 +0900

    feat: add search box title

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -5,4 +5,17 @@
   const { data }: PageProps = $props();
 </script>
 
+<style lang="scss">
+  .h1 {
+    font-size: 1.5rem;
+    font-weight: 900;
+    color: #404040;
+    text-align: center;
+
+    margin-bottom: 1.2rem;
+  }
+</style>
+
+<h1 class="h1">求人検索</h1>
+
 <JobList jobs={ data.jobs } />
