03b0246aba654db698453f5e0c9e3a82d2ae6e5d
Jumpei Ogawa <git@phanective.org>
2025-03-22 11:38:50 +0900

    refactor: extract `<JobList>`

---
+++ src/lib/components/JobList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import JobCard from "$lib/components/JobCard.svelte";
+  import type { Job } from "@prisma/client";
+
+  type Props = {
+    jobs: Job[];
+    editable?: boolean;
+  };
+
+  const { jobs, editable = false }: Props = $props();
+</script>
+
+<section class="p-24 flex flex-col">
+  <div class="grid grid-cols-2 justify-between gap-6">
+    {#each jobs as job (job.id)}
+      <JobCard { job } { editable } />
+    {/each}
+  </div>
+</section>
