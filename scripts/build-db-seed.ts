40b8a35a787db1cee7cff00e4f57cdff3e505542
Jumpei Ogawa <git@phanective.org>
2025-03-22 11:20:08 +0900

    chore: rewrite seed data in English and add more jobs

--- scripts/build-db-seed.ts
+++ scripts/build-db-seed.ts
@@ -4,13 +4,13 @@ import { hashPassword } from "../src/lib/utils/bcrypt.utils.ts";
 
 const candidates: Parameters<typeof prisma.user.upsert>[0]["create"][] = [
   {
-    full_name: "神里 綾華",
+    full_name: "Ayaka Kamisato",
     email: "ayaka.kamisato@phanective.org",
     password: await hashPassword("Pa$$w0rd!"),
     role: "CANDIDATE",
   },
   {
-    full_name: "久岐忍",
+    full_name: "Shinobu Kuki",
     email: "kuki@phanective.org",
     password: await hashPassword("Pa$$w0rd!"),
     role: "CANDIDATE",
@@ -23,50 +23,143 @@ const candidates: Parameters<typeof prisma.user.upsert>[0]["create"][] = [
   },
 ];
 
-const employer = {
-  id: "yaemiko",
-  full_name: "八重 神子",
-  email: "miko.yae@phanective.org",
-  password: await hashPassword("Pa$$w0rd!"),
-  role: "EMPLOYER",
-} as const satisfies Parameters<typeof prisma.user.upsert>[0]["create"];
+const employers = [
+  {
+    id: "yaemiko",
+    full_name: "Miko Yae",
+    email: "miko.yae@phanective.org",
+    password: await hashPassword("Pa$$w0rd!"),
+    role: "EMPLOYER",
+  } as const satisfies Parameters<typeof prisma.user.upsert>[0]["create"],
+  {
+    id: "raidenei",
+    full_name: "Ei Raiden",
+    email: "ei.raiden@inazuma.gov",
+    password: await hashPassword("Pa$$w0rd!"),
+    role: "EMPLOYER",
+  } as const satisfies Parameters<typeof prisma.user.upsert>[0]["create"],
+];
 
-const jobs: Parameters<typeof prisma.job.create>[0]["data"][] = [{
-  id: "dummy-job-1",
-  title: "【フルリモート】TypeScript エンジニア (Svelte / Hono / React Native)",
-  description: `
-弊社オンライン書店のフロントエンド、バックエンド、モバイルアプリのうちいずれかまたは複数に関して開発を担当するソフトウェアエンジニアを募集します。
-…
-`.trim(),
-  requirements: `
-# 必須要件
+const jobs: Parameters<typeof prisma.job.create>[0]["data"][] = [
+  {
+    id: "dummy-job-1",
+    title: "[WFH] TypeScript Developer for our ebook store (Svelte / Hono / React Native)",
+    description: `
+      We are hiring a software developer for our ebook store. Your will be responsibile for one or some of the frontend, backend, and mobile app development.
+      ...
+    `.trim(),
+    requirements: `
+      # Required
 
-- TypeScript の経験2年以上
-- Svelte、React、Vue.js、Angular などのコンポーネント志向フロントエンドフレームワークの経験1年以上
+      - One year or more experience in TypeScript development
+      - Two years or more experience in JavaScript or altJS (e.g. TypeScript, Elm, CoffeeScript)
+      - One year or more experience in one of the followings:
+        - Svelte, React, Vue.js, Angular, or any other component-oriented frontend framework(s)
+        - Node.js, Cloudflare Workers, or any other backend JavaScript/TypeScript framework(s)/platform(s)
+        - React Native or any other mobile JavaScript/TypeScript framework(s)
+    `.trim(),
+    responsibilities: `
+      You will be responsible for the one or more of the followings:
 
-# 歓迎要件
+      - Web frontend development for our ebook store (Svelte)
+      - Backend development for our ebook store (Hono + Cloudflare Workers)
+      - Mobile app development for our ebook store (React Native)
+    `.trim(),
+    applicationUrl: "https://phanective.org/job-example-1",
+    location: "Remote (any location in Inazuma)",
+    salary: "8,000,000 Mora/year",
+    company: "Yae Publishing House K.K.",
+    deadline: dayjs().add(1, "month").toDate(),
+    employerId: employers[0].id,
+  },
+  {
+    id: "dummy-job-2",
+    title: "[WFH] SRE for our ebook store",
+    description: `
+      We are hiring an SRE for our ebook store.
+      ...
+    `.trim(),
+    requirements: `
+      # Required
 
-- Node.js や WinterCG 互換プラットフォームなど、バックエンド JavaScript/TypeScript の経験
-- フロントエンドとバックエンド (JavaScript/TypeScript 以外の言語によるものも含む) 両方の経験
-`.trim(),
-  responsibilities: `
-弊社サービス ○○ の開発のうち、以下の1つまたはそれ以上の領域 (担当領域はご希望やご経験に応じて調整させて頂きます)
-- ○○ のフロントエンド開発 (Svelte)
-- ○○ のバックエンド開発 (Hono + Cloudflare Workers)
-- ○○ のモバイルアプリ開発 (React Native: Android 及び iOS)
-`.trim(),
-  applicationUrl: "https://phanective.org/job-example",
-  location: "フルリモート勤務につき稲妻国内の任意の場所",
-  salary: "年収800万円",
-  company: "株式会社八重堂",
-  deadline: dayjs().add(1, "month").toDate(),
-  employerId: employer.id,
-}];
+      - Two year or more experience as an SRE or an infrastructure engineer.
+      - One year or more experience in the production Docker server administration
+      - One year or more experience in Infrastructure as Code tools such as Terraform and Ansible
+
+      # Nice to have
+
+      - Experience in Node.js server administration
+    `.trim(),
+    responsibilities: `
+      You will be responsible for the infrastructure administration and the site reliability engineering for our online ebook store.
+    `.trim(),
+    applicationUrl: "https://phanective.org/job-example-2",
+    location: "Remote (any location in Inazuma)",
+    salary: "8,000,000 Mora/year",
+    company: "Yae Publishing House K.K.",
+    deadline: dayjs().add(1, "month").toDate(),
+    employerId: employers[0].id,
+  },
+  {
+    id: "dummy-job-3",
+    title: "[WFH] Marketing lead",
+    description: `
+      We are hiring a marketing lead for our ebook store.
+      ...
+    `.trim(),
+    requirements: `
+      # Required
+
+      - Two year or more experience as a digital marketer in B2C field
+
+      # Nice to have
+
+      - Basic understandings for HTML
+    `.trim(),
+    responsibilities: `
+      - Marketing for our ebook store
+      - Marketers management
+    `.trim(),
+    applicationUrl: "https://phanective.org/job-example-3",
+    location: "Remote (any location in Inazuma)",
+    salary: "8,000,000 Mora/year",
+    company: "Yae Publishing House K.K.",
+    deadline: dayjs().add(1, "month").toDate(),
+    employerId: employers[0].id,
+  },
+  {
+    id: "dummy-job-4",
+    title: "Corporate Engineer",
+    description: `
+      We are hiring a software engineer for the Shogunate's internal systems.
+      ...
+    `.trim(),
+    requirements: `
+      # Required
+
+      - Two or more experience in Java
+
+      # Nice to have
+
+      - Experience as a corporate IT admin
+    `.trim(),
+    responsibilities: `
+      - Development for the Shogunate's internal systems
+      - Vendor management for the Shogunate's internal systems
+    `.trim(),
+    applicationUrl: "https://phanective.org/job-example-4",
+    location: "The Inazuma Castle, 1-1-1, Inazuma City, Narukami Island, Inazuma",
+    salary: "9,000,000 Mora/year",
+    company: "Tenryou Commission, The Shogunate of Inazuma",
+    deadline: dayjs().add(1, "month").toDate(),
+    employerId: employers[1].id,
+  },
+];
 
 const prisma = new PrismaClient();
 
 // Allow N+1 problem here since we don't have to be serious for performance here.
-for (const user of [ ...candidates, employer ]) {
+for (const user of [ ...candidates, ...employers ]) {
   await prisma.user.upsert({
     where: {
       email: user.email,
