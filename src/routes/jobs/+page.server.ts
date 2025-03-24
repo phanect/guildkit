c4e4e85ed75ab5f3ac2575f3639722828948a08f
Jumpei Ogawa <git@phanective.org>
2025-03-24 11:42:02 +0900

    refactor: set return type for load function

--- src/routes/jobs/+page.server.ts
+++ src/routes/jobs/+page.server.ts
@@ -1,7 +1,8 @@
 import prisma from "$lib/prisma.ts";
+import type { Job } from "@prisma/client";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad<{ jobs: Job[]; }> = async () => {
   const today = new Date();
   const response = await prisma.job.findMany({
     where: {
