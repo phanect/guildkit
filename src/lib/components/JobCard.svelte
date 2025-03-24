13036b0ba501d0b0dbcb58c0aeeea3b454543b0f
Jumpei Ogawa <git@phanective.org>
2025-03-24 12:15:48 +0900

    feat: update layout for `JobList` & `JobCard`s

--- src/lib/components/JobCard.svelte
+++ src/lib/components/JobCard.svelte
@@ -18,34 +18,90 @@
   const { job, editable = false }: Props = $props();
 </script>
 
-<div class="overflow-hidden group relative rounded-lg p-[1px] flex justify-center items-center">
-  <a class="block w-full relative z-10 rounded-lg bg-white p-6 sm:p-8" href="jobs/{job.id}">
-    <div class="mt-12 sm:pr-8">
-      <h3 class="text-xl font-bold text-gray-900">
-        {job.title}
-      </h3>
-      <p class="mt-2 text-sm text-gray-500 truncate">
-        {job.description}
-      </p>
-      <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-      <div class="text-sm flex justify-between">
-        <p class="text-gray-700">Deadline</p>
-        <div class="flex flex-col items-end">
-          <p class="text-gray-400">{job.deadline.toLocaleDateString()}</p>
-          <p class="text-red-300">({getRemainingTime(job.deadline)})</p>
-        </div>
+<style>
+  .card {
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.75rem;
 
-        {#if editable}
-          <div class="flex items-end gap-2">
-            <Button href={`/employer/jobs/edit/${ job.id }`} preload={true}>
-              Edit
-            </Button>
-            <Button action="/employer/jobs?/delete" params={{ id: job.id }}>
-              Delete
-            </Button>
-          </div>
-        {/if}
-      </div>
+    background-color: #ffffff;
+    box-shadow: 2px 2px 5px 5px #f0f0f0;
+    transition: box-shadow 0.25s;
+    border-radius: 0.5rem;
+
+    width: 100%;
+    max-width: 32.5rem;
+    padding: 1rem;
+  }
+
+  .card:has(.card-link:hover) {
+    box-shadow: 4px 4px 10px 7px #d0d0d0;
+  }
+
+  .card-link {
+    cursor: pointer;
+  }
+
+  .card-title {
+    font-size: 1.25rem;
+    font-weight: 700;
+    color: #101828;
+
+    overflow: hidden;
+    line-height: 1.5em;
+    height: 3em; // = (text height 2em) + (space between lines 0.5em) + (accomodation 0.5em)
+  }
+
+  .card-desc {
+    font-size: 0.875rem;
+    color: #363636;
+
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    height: 3em; // = (text height 2em) + (space between lines 0.5em) + (accomodation 0.5em)
+  }
+
+  .actions {
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    column-gap: 0.5rem;
+
+    font-size: 0.875rem;
+  }
+
+  .expire-text {
+    color: #364153;
+  }
+</style>
+
+<div class="card">
+  <a class="card-link" href="jobs/{job.id}">
+    <h3 class="card-title">
+      {job.title}
+    </h3>
+    <div class="card-desc">
+      {job.description}
     </div>
   </a>
+
+  <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+
+  <div class="actions">
+    <div class="expire-text">
+      Deadline: {job.deadline.toLocaleDateString()} <span class="text-red-300">({getRemainingTime(job.deadline)})</span>
+    </div>
+
+    {#if editable}
+      <div class="flex items-end gap-2">
+        <Button href={`/employer/jobs/edit/${ job.id }`} preload={true}>
+          Edit
+        </Button>
+        <Button action="/employer/jobs?/delete" params={{ id: job.id }}>
+          Delete
+        </Button>
+      </div>
+    {/if}
+  </div>
 </div>
--- src/lib/components/JobList.svelte
+++ src/lib/components/JobList.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import JobCard from "$lib/components/JobCard.svelte";
   import type { Job } from "@prisma/client";
+  import JobCardEmpty from "./JobCardEmpty.svelte";
 
   type Props = {
     jobs: Job[];
@@ -10,10 +11,25 @@
   const { jobs, editable = false }: Props = $props();
 </script>
 
-<section class="p-24 flex flex-col">
-  <div class="grid grid-cols-2 justify-between gap-6">
-    {#each jobs as job (job.id)}
-      <JobCard { job } { editable } />
-    {/each}
-  </div>
+<style>
+  .joblist {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    column-gap: 2.5em;
+    row-gap: 2.5rem;
+
+    max-width: 100%;
+    width: fit-content;
+  }
+</style>
+
+<section class="joblist">
+  {#each jobs as job (job.id)}
+    <JobCard { job } { editable } />
+  {/each}
+
+  {#if jobs.length % 2 === 1}
+    <JobCardEmpty />
+  {/if}
 </section>
