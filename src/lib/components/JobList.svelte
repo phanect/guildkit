40971269ce47926be9926fb377d9ae8b4e22d0e7
Jumpei Ogawa <git@phanective.org>
2025-03-24 12:15:48 +0900

    feat: update layout for `JobList` & `JobCard`s

--- src/lib/components/JobList.svelte
+++ src/lib/components/JobList.svelte
@@ -10,10 +10,21 @@
   const { jobs, editable = false }: Props = $props();
 </script>
 
-<section class="p-24 flex flex-col">
-  <div class="grid grid-cols-2 justify-between gap-6">
-    {#each jobs as job (job.id)}
-      <JobCard { job } { editable } />
-    {/each}
-  </div>
+<style>
+  .joblist {
+    display: flex;
+    justify-content: start;
+    align-items: flex-start;
+    column-gap: 2.5em;
+    row-gap: 2.5rem;
+
+    max-width: 96rem;
+    width: 100%;
+  }
+</style>
+
+<section class="joblist">
+  {#each jobs as job (job.id)}
+    <JobCard { job } { editable } />
+  {/each}
 </section>
