0313ffe5fc7ba37671c79b9e4e4fd65af30c742b
Jumpei Ogawa <git@phanective.org>
2025-03-25 09:45:32 +0900

    refactor: check employers' auth token in the layout, not in the hooks

---
+++ src/routes/employer/+layout.server.ts
@@ -0,0 +1,10 @@
+import { redirect } from "@sveltejs/kit";
+import type { LayoutServerLoad } from "./$types";
+
+export const load: LayoutServerLoad = async ({ cookies }) => {
+  const token = cookies.get("token");
+
+  if (!token) {
+    return redirect(303, "/auth/login");
+  }
+};
