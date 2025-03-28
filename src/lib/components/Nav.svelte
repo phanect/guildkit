3a107f4d858e194fd01b643d09442a5c801d6f5e
Jumpei Ogawa <git@phanective.org>
2025-03-28 17:03:40 +0900

    feat: polish `<Nav />` layout
    - add placeholder icon on tmp.guildkit.net
    - add gaps
    - remove redundant `alt`. Since there is the same text near the icon, `alt` should be removed in this case.

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -12,14 +12,36 @@
   .nav {
     padding: 2rem;
   }
+
+  .title {
+    display: flex;
+    align-items: center;
+    column-gap: 0.75rem;
+  }
+  .title-text {
+    font-size: 1.25rem;
+    font-weight: 600;
+  }
+
+  .right-section {
+    display: flex;
+    align-items: center;
+    column-gap: 1rem;
+  }
 </style>
 
 <nav class="nav flex items-center justify-between flex-wrap py-6 px-20">
-  <div class="flex items-center shrink-0 mr-6">
-    <img src="/images/to-be-added.svg" alt="GuildKit" decoding="async" />
-    <a href="/" class="font-semibold text-xl tracking-tight">GuildKit</a>
-  </div>
-  <div class="flex items-center">
+  <a href="/" class="title">
+    <img
+      src="https://tmp.guildkit.net/canvaai/guildkit_icon_tmp.png"
+      width="64"
+      height="64"
+      alt=""
+      decoding="async"
+    />
+    <span class="title-text">GuildKit</span>
+  </a>
+  <div class="right-section">
     {#if !isLoggedIn}
       <Button href="/auth/signup">Sign up</Button>
     {:else}
