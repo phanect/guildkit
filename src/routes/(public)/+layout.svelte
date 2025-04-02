ee377fd751660fe71b590b8f650767549da8d3b4
Jumpei Ogawa <git@phanective.org>
2025-04-02 10:27:41 +0900

    feat: update styles for layouts

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -6,7 +6,20 @@
   const { data, children }: LayoutProps = $props();
 </script>
 
+<style>
+  .main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    row-gap: 1em;
+
+    width: 100%;
+    padding-left: 2.5em;
+    padding-right: 2.5em;
+  }
+</style>
+
 <Nav isLoggedIn={data.isLoggedIn} />
-<main>
+<main class="main">
   {@render children()}
 </main>
