5e89b3cac7a5fe2d8d665a0eaac8b42ac0893b30
Jumpei Ogawa <git@phanective.org>
2025-04-08 07:34:36 +0900

    chore: switch to PostgreSQL from SQLite

---
+++ scripts/prepare-pg-install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -eux
+
+DIRNAME="$(realpath "$(dirname  -- "${BASH_SOURCE[0]}")")"
+PROJECT_ROOT="$(realpath "${DIRNAME}/..")"
+PGDATA_ROOT="${PROJECT_ROOT}/tmp/pgdata"
+
+mise up
+eval "$(mise activate bash)"
+
+if [[ ! -d "${PGDATA_ROOT}" ]]; then
+  mkdir --parents "${PGDATA_ROOT}"
+  initdb --pgdata="${PGDATA_ROOT}" --username=postgres
+
+  pnpm db:restart
+
+  psql --username=postgres --command="CREATE USER guildkit WITH CREATEDB PASSWORD 'guildkit';"
+  psql --username=postgres --command="CREATE DATABASE guildkit OWNER = guildkit;"
+  psql --username=postgres --command="GRANT ALL PRIVILEGES ON DATABASE guildkit TO guildkit;"
+else
+  pnpm db:restart
+fi
