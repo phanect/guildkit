a1e819d946c9b9589dc00b605af95a4b135b5eb0
Jumpei Ogawa <git@phanective.org>
2025-04-11 13:35:09 +0900

    wip

--- scripts/prepare-pg.sh
+++ scripts/prepare-pg.sh
@@ -7,19 +7,21 @@ set -eu
 type apt-get &> /dev/null
 (ls /etc/apt/sources.list && cat /etc/apt/sources.list | grep ubuntu.com) &> /dev/null
 
-if dpkg -l "linux-headers-$(uname -r)" build-essential libssl-dev libreadline-dev zlib1g-dev libcurl4-openssl-dev uuid-dev icu-devtools libicu-dev libbison-dev flex > /dev/null; then
-  exit 0
+if ! dpkg -l "linux-headers-$(uname -r)" build-essential libssl-dev libreadline-dev zlib1g-dev libcurl4-openssl-dev uuid-dev icu-devtools libicu-dev libbison-dev flex > /dev/null; then
+  echo "Installing build dependencies for PostgreSQL from apt repository. Enter your sudo password to install."
+
+  sudo apt-get install linux-headers-$(uname -r) build-essential libssl-dev libreadline-dev zlib1g-dev libcurl4-openssl-dev uuid-dev icu-devtools libicu-dev libbison-dev flex
+
+  echo "Installing PostgreSQL..."
 fi
 
-echo "Installing build dependencies for PostgreSQL from apt repository. Enter your sudo password to install."
-
-sudo apt-get install linux-headers-$(uname -r) build-essential libssl-dev libreadline-dev zlib1g-dev libcurl4-openssl-dev uuid-dev icu-devtools libicu-dev libbison-dev flex
-
-echo "Installing PostgreSQL..."
-
 mise up
+mise activate bash
 
 pg_ctl restart
-psql --username=postgres --command="CREATE USER guildkit;" || :
+psql --username=postgres --command="CREATE USER guildkit;" # || :
 psql --username=postgres --command="ALTER USER guildkit WITH PASSWORD 'guildkit';"
-createdb guildkit --username=postgres --owner=guildkit || :
+psql --username=postgres --command="ALTER USER guildkit CREATEDB;" # Required only on local machine
+psql --username=guildkit --command="CREATE DATABASE guildkit;"
+# createdb guildkit --username=guildkit --owner=guildkit # || :
+psql --username=postgres --command="GRANT ALL PRIVILEGES ON DATABASE guildkit TO guildkit;"
