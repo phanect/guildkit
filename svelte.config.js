317a45b48019ad497711a99682713de7ffd1296c
Jumpei Ogawa <git@phanective.org>
2025-04-17 16:51:36 +0900

    fix: switch to node adapter from auto adapter

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import deepmerge from "deepmerge";
 import { sveltePreprocess } from "svelte-preprocess";
 import { preprocessMeltUI, sequence } from "@melt-ui/pp";
 import tsconfigPhanective from "@phanect/configs/ts/importable";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type { import("@sveltejs/kit").Config } */
