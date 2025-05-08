099f9b63b9ff3b12acb429a4f615a9bbf7888ded
Jumpei Ogawa <git@phanective.org>
2025-05-08 20:12:57 +0900

    fix: explicitly declare exit code
    Without this, the command automatically exits with exit code 1.

--- scripts/scripts-prepare-pg-deps.sh
+++ scripts/scripts-prepare-pg-deps.sh
@@ -9,11 +9,11 @@ type mise
 
 # If this script runs on Ubuntu or Ubuntu-based systems, install dependencies required to build Postgres
 
-type apt-get > /dev/null || exit
-type dpkg > /dev/null || exit
-(cat /etc/apt/sources.list /etc/apt/sources.list.d/* | grep "archive.ubuntu.com") > /dev/null || exit
+type apt-get > /dev/null || exit 0
+type dpkg > /dev/null || exit 0
+(cat /etc/apt/sources.list /etc/apt/sources.list.d/* | grep "archive.ubuntu.com") > /dev/null || exit 0
 
-dpkg -l "linux-headers-$(uname -r)" build-essential libssl-dev libreadline-dev zlib1g-dev libcurl4-openssl-dev uuid-dev icu-devtools libicu-dev libbison-dev flex > /dev/null && exit
+dpkg -l "linux-headers-$(uname -r)" build-essential libssl-dev libreadline-dev zlib1g-dev libcurl4-openssl-dev uuid-dev icu-devtools libicu-dev libbison-dev flex > /dev/null && exit 0
 
 echo "Installing build dependencies for PostgreSQL from apt repository. Enter your sudo password to install."
 
