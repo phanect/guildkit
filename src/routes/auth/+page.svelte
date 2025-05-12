eda719f975a725938f078e5ab1db51c84a4929c3
Jumpei Ogawa <git@phanective.org>
2025-05-12 16:38:23 +0900

    perf: remove Google Font only for /auth

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -9,12 +9,6 @@
 </svelte:head>
 
 <style lang="scss">
-  @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
-
-  * {
-    font-family: "Montserrat", sans-serif;
-  }
-
   h1 {
     font-size: 1.75rem;
   }
