95bc7f0f770a7cedb0edf50d7a845c78dc1900b6
Jumpei Ogawa <git@phanective.org>
2025-05-13 16:39:51 +0900

    feat: redirect if already logged-in when the user visit /auth

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,1 +1,19 @@
-export const load = async () => {};
+import { auth } from "$lib/auth.ts";
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ request }) => {
+  const { user } = await auth.api.getSession({
+    headers: request.headers,
+  }) ?? {};
+
+  if (user) {
+    if (user.type === "candidate") {
+      return redirect(307, "/");
+    } else if (user.type === "recruiter") {
+      return redirect(307, "/employer");
+    } else if (user.type === "administrative") {
+      return redirect(307, "/"); // TODO redirect to the better path
+    }
+  }
+};
