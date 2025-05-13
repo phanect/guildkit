42a41d8a19f2424c5f82097af73c5bc1d2fe0fc0
Jumpei Ogawa <git@phanective.org>
2025-05-13 16:39:51 +0900

    feat: redirect if already logged-in when the user visit /auth

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,63 +1,24 @@
-import { error, fail, redirect } from "@sveltejs/kit";
-import { superValidate } from "sveltekit-superforms/server";
-import jwt from "jsonwebtoken";
+import { getSession } from "$lib/auth.ts";
+import { redirect } from "@sveltejs/kit";
+import { superValidate } from "sveltekit-superforms";
 import { loginSchema } from "$lib/validation/user.validation.ts";
-import { isRegistered } from "$lib/utils/user.utils.ts";
-import { isValidPassword } from "$lib/utils/bcrypt.utils.ts";
-import prisma from "$lib/prisma.ts";
-import type { RequestEvent } from "./$types";
-import { JWT_SECRET } from "$env/static/private";
+import type { PageServerLoad } from "./$types";
 
-export const load = async (event: RequestEvent) => {
-  const form = await superValidate(event, loginSchema);
+export const load: PageServerLoad = async ({ request }) => {
+  const { user } = await getSession({
+    headers: request.headers,
+  }) ?? {};
+
+  if (user) {
+    if (user.roles.includes("candidate")) {
+      return redirect(307, "/");
+    } else if (user.roles.includes("recruiter") || user.roles.includes("recruiterOrgOwner")) {
+      return redirect(307, "/employer");
+    } else if (user.roles.includes("siteOwner") || user.roles.includes("admin")) {
+      return redirect(307, "/"); // TODO redirect to the better path
+    }
+  }
+
+  const form = await superValidate(loginSchema);
   return { form };
-};
-
-export const actions = {
-  default: async (event: RequestEvent) => {
-    const form = await superValidate(event, loginSchema);
-
-    if (!form.valid) {
-      return fail(400, { form });
-    }
-
-    if (!(await isRegistered(form.data.email))) {
-      return error(400, "User not found");
-    }
-
-    const user = await prisma.user.findUnique({
-      where: {
-        email: form.data.email,
-      },
-    });
-
-    if (!user || !(await isValidPassword(form.data.password, user.password))) {
-      return error(400, "Invalid email or password");
-    }
-
-    const userData = {
-      id: user.id,
-      role: user.role,
-      email: user.email,
-      fullname: user.fullname,
-    };
-
-    const token = jwt.sign(userData, JWT_SECRET, { expiresIn: "1d" });
-    event.cookies.set("token", token, {
-      httpOnly: true,
-      path: "/",
-      sameSite: true,
-      maxAge: 60 * 60 * 24,
-    });
-
-    if (user.role === "CANDIDATE") {
-      return redirect(303, "/");
-    } else if (user.role === "RECRUITER") {
-      return redirect(303, "/employer/jobs");
-    } else if (user.role === "ADMIN") {
-      return redirect(303, "/users");
-    } else {
-      throw new Error("Unexpected user role");
-    }
-  },
 };
