ced990f049b34d0d9b791db7f53f270ff98603f2
Jumpei Ogawa <git@phanective.org>
2025-05-18 14:29:21 +0900

    chore: rewrite database seed script with better-auth

--- src/lib/utils/bcrypt.utils.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export const hashPassword = (password: string) => {
-  return bcrypt.hash(password, 12);
-};
-
-export const isValidPassword = async (providedPassword: string, userPassword: string) =>
-  bcrypt.compare(providedPassword, userPassword);
