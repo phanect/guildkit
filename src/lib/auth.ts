44ef918319fd181916fee9b98116120d649b10fc
Jumpei Ogawa <git@phanective.org>
2025-05-18 16:30:31 +0900

    refactor: migrate `UserRole`s to `UserType`s & add better-auth's ACL
    Adding better-auth's organization and admin plugins.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { env } from "node:process";
 import { betterAuth } from "better-auth";
+import { admin as adminPlugin, organization } from "better-auth/plugins";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { adminAc, adminRoles, recruiterAc, recruiterRoles } from "./auth/roles.ts";
 import prisma from "./prisma.ts";
 
 if (
@@ -13,18 +15,38 @@ if (
     Did you set all of the { GOOGLE | GITHUB }_CLIENT_ID and {GOOGLE | GITHUB }_CLIENT_SECRET?`);
 }
 
+//
+// auth setup
+//
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   user: {
     additionalFields: {
-      role: {
-        type: [ "ADMIN", "RECRUITER", "CANDIDATE" ],
-        required: true,
+      type: {
+        type: [ "candidate", "recruiter", "administrative" ],
+        required: false,
+        input: true,
       },
     },
   },
+  plugins: [
+    organization({
+      defaultRole: "recruiter",
+      adminRoles: [ "recruiterAdmin" ],
+      creatorRole: "recruiterAdmin",
+      ac: recruiterAc,
+      roles: recruiterRoles,
+    }),
+    adminPlugin({
+      defaultRole: "none",
+      adminRoles: [ "gkAdmin" ],
+      defaultBanReason: "Unspecified",
+      ac: adminAc,
+      roles: adminRoles,
+    }),
+  ],
   socialProviders: {
     google: {
       prompt: "select_account",
