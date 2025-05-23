7ece5d2d4a453c8c899acc5fd914c65f9e095450
Jumpei Ogawa <git@phanective.org>
2025-05-24 08:26:22 +0900

    chore: add com.chrome.devtools.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     sveltekit(),
     tailwindcss(),
+    devtoolsJson(),
   ],
   server: {
     port: 3000,
