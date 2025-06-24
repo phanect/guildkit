7d8dfe2197180f51da33c5ddc928ec267fc9b71c
Jumpei Ogawa <git@phanective.org>
2025-06-24 12:05:46 +0900

    feat: remove email & password authentication
    I will add Magic Link as a login method later.

--- src/lib/validation/user.validation.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-import { zod as adaptSuperForms } from "sveltekit-superforms/adapters";
-
-export const loginSchema = adaptSuperForms(z.object({
-  email: z.string().email(),
-  password: z.string(),
-}));
