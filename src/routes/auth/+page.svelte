436e13ab78f88ec081e4b975e9e81f820fe17e80
Jumpei Ogawa <git@phanective.org>
2025-06-24 12:05:46 +0900

    feat: remove email & password authentication
    I will add Magic Link as a login method later.

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,25 +1,6 @@
 <script lang="ts">
-  import { superForm } from "sveltekit-superforms/client";
   import { signInWith } from "$lib/auth/client.ts";
   import Button from "$lib/components/generic/Button.svelte";
-  import Toast, { addToast } from "$lib/components/generic/Toast.svelte";
-  import { loginSchema } from "$lib/validation/user.validation.ts";
-  import type { PageProps } from "./$types";
-
-  const { data }: PageProps = $props();
-
-  const { form, errors, enhance } = superForm(data.form, {
-    validators: loginSchema,
-    onError: (error) => {
-      addToast({
-        data: {
-          title: "Error",
-          description: error.result.error.message,
-          color: "variant-filled-warning",
-        },
-      });
-    },
-  });
 </script>
 
 <style lang="scss">
@@ -31,7 +12,6 @@
   }
 </style>
 
-<Toast />
 <section
   class="border border-gray-300 w-fit shadow-md mx-auto align-middle p-5 rounded-sm font-roboto h-fit my-auto mt-32"
 >
@@ -46,36 +26,4 @@
       Signin with GitHub
     </Button>
   </div>
-
-  <form method="post" use:enhance>
-    <div>
-      <label class="label" for="email">
-        <span>Email</span>
-        <input
-          class={$errors.email ? "input-error block w-full" : "input"}
-          type="email"
-          placeholder="johndoe@example.com"
-          name="email"
-          id="email"
-          bind:value={$form.email}
-        />
-      </label>
-      {#if $errors.email}
-        <small class="text-red-600">{$errors.email}</small>
-      {/if}
-    </div>
-    <div>
-      <label class="label" for="password">
-        <span>Password</span>
-        <input
-          class="input"
-          type="password"
-          placeholder="•••••••••"
-          name="password"
-          id="password"
-        />
-      </label>
-    </div>
-    <button type="submit" class="btn variant-filled w-full mt-8 mb-8">Log in</button>
-  </form>
 </section>
