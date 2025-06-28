f35977a5d01b0ac356e4e967863a28d5dcd6e909
Jumpei Ogawa <git@phanective.org>
2025-06-28 15:03:12 +0900

    feat: rewrite auth process in /employer/jobs with better-auth

--- src/routes/employer/jobs/+page.server.ts
+++ src/routes/employer/jobs/+page.server.ts
@@ -1,51 +1,44 @@
 import { fail, redirect } from "@sveltejs/kit";
-import jwt from "jsonwebtoken";
 import { superValidate } from "sveltekit-superforms/server";
 import prisma from "$lib/prisma.ts";
+import { requireAuthAs } from "$lib/auth.ts";
 import { jobSchema } from "$lib/validation/job.validation.ts";
 import type { PageServerLoad, RequestEvent } from "./$types";
-import { JWT_SECRET } from "$env/static/private";
 
-export const load: PageServerLoad = async ({ cookies }) => {
-  const token = cookies.get("token");
-  if (typeof token !== "string") {
-    return fail(400, { reason: "Invalid token" });
-  }
-  const decodedToken = jwt.verify(token, JWT_SECRET) as { role: UserRole; id: string; };
-  const { id, role } = decodedToken;
+export const load: PageServerLoad = async ({ parent }) => {
+  const { user } = await parent();
 
-  if (role === "RECRUITER") {
-    return {
-      jobs: await prisma.job.findMany({
-        where: {
-          employerId: id,
-        },
-      }),
-      role,
-    };
-  } else if (role === "CANDIDATE" || role === "ADMIN") {
-    return fail(400, { reason: "Unauthorized" });
-  } else {
-    console.error(`Unexpected \`role\` value: "${ role as string }"`);
-    return fail(400, { reason: "Unauthorized" });
-  }
+  return {
+    jobs: await prisma.job.findMany({
+      where: {
+        employerId: user.id,
+      },
+    }),
+    type: user.type,
+  };
 };
 
 export const actions = {
   create: async (event: RequestEvent) => {
+    const { user } = await requireAuthAs("recruiter", { request: event.request });
+    const employerId = user?.id;
+
+    if (!employerId) {
+      return fail(401, {
+        error: "Not authenticated as a recruiter.",
+      });
+    }
+
     const form = await superValidate(event, jobSchema);
+
     if (!form.valid) {
       return fail(400, { form });
     }
-    const token = event.cookies.get("token");
-    if (typeof token !== "string") {
-      return fail(400, { error: "Invalid token" });
-    }
-    const decodedToken = jwt.verify(token, JWT_SECRET) as { id: string; };
-    const { id } = decodedToken;
+
     await prisma.job.create({
-      data: { ...form.data, employerId: id },
+      data: { ...form.data, employerId },
     });
+
     redirect(303, "/employer/jobs");
   },
   delete: async (event: RequestEvent) => {
