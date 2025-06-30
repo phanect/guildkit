bf5347bf3bf6d21c7d5f29c8f899ae520078bf99
Jumpei Ogawa <git@phanective.org>
2025-06-30 11:42:15 +0900

    refactor: migrate to Drizzle ― schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   dialect: "postgresql",
   out: "./src/lib/db/migrations",
+  schema: "./src/lib/db/schema.ts",
   strict: true,
   verbose: true,
   dbCredentials: {
