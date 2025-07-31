5913374fd2542fb004208f357be534e31f05320a
Jumpei Ogawa <git@phanective.org>
2025-07-31 17:43:14 +0900

    chore: add `building` to BetterAuth hook
    This is now required by BetterAuth's minor update

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,7 @@
 import { svelteKitHandler } from "better-auth/svelte-kit";
+import { building } from "$app/environment";
 import { auth } from "$lib/auth.ts";
 import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) =>
-  svelteKitHandler({ event, resolve, auth }) as Promise<Response>;
+  svelteKitHandler({ event, resolve, auth, building }) as Promise<Response>;
