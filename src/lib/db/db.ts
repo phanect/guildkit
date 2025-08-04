6842ccdb4c6321139aeab2861c22e47ee7098f23
Jumpei Ogawa <git@phanective.org>
2025-08-04 09:49:47 +0900

    refactor: enable `db.query.find*()` functions

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,22 +1,23 @@
 import "dotenv/config";
 import { eq, type InferInsertModel } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { userPropsTable, type userTable } from "./schema.ts";
+import * as schema from "./schema.ts";
 
 export const db = drizzle({
   connection: {
     connectionString: process.env.DATABASE_URL!,
   },
+  schema,
 });
 
-export const updateUserProps = (user: InferInsertModel<typeof userTable>) => {
-  type UpdateFn = typeof db.update<typeof userPropsTable>;
+export const updateUserProps = (user: InferInsertModel<typeof schema.userTable>) => {
+  type UpdateFn = typeof db.update<typeof schema.userPropsTable>;
   type SetFn = ReturnType<UpdateFn>["set"];
   type SetParams = Parameters<SetFn>;
 
   return {
-    set: async (...values: SetParams) => db.update(userPropsTable)
+    set: async (...values: SetParams) => db.update(schema.userPropsTable)
       .set(...values)
-      .where(eq(userPropsTable.id, user.propsId)),
+      .where(eq(schema.userPropsTable.id, user.propsId)),
   };
 };
