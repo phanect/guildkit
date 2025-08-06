25ca42999a6481054662b9e31ca6ee7a2fac7b1e
Jumpei Ogawa <git@phanective.org>
2025-08-06 18:45:59 +0900

    chore: rewrite seed script to add organizations

--- src/lib/db/helpers.ts
+++ src/lib/db/helpers.ts
@@ -3,9 +3,11 @@ import { eq, type InferInsertModel } from "drizzle-orm";
 import { db } from "./db.ts";
 import { user as userTable } from "./schema/better-auth.ts";
 import { userProps } from "./schema/user.ts";
+import { organizationsAndRecruitersRelationTable } from "./schema/relations.ts";
 
-export type UserWithProps = Omit<InferInsertModel<typeof userTable>, "propsId"> & {
+export type UserWithProps = Omit<InferInsertModel<typeof userTable>, "propsId" | "recruitsFor"> & {
   props: InferInsertModel<typeof userProps>;
+  recruitsFor?: string[];
 };
 
 export const insertUsers = async (users: UserWithProps | UserWithProps[]) => {
@@ -14,9 +16,21 @@ export const insertUsers = async (users: UserWithProps | UserWithProps[]) => {
   // TODO Fix N+1 problem
   await db.transaction(async (tx) => {
     for (const userWithProps of usersWithProps) {
-      const { props, ...userWithoutProps } = userWithProps;
+      const { props, recruitsFor: orgIds = [], ...userWithoutProps } = userWithProps;
 
       const [{ propsId }] = await tx.insert(userProps).values(props).returning({ propsId: userProps.id });
+
+      if (0 < orgIds.length) {
+        tx.insert(organizationsAndRecruitersRelationTable).values(orgIds.map((orgId) => ({
+          organizationId: orgId,
+          recruiterId: userWithoutProps.id,
+        }))).onConflictDoNothing({
+          target: [
+            organizationsAndRecruitersRelationTable.organizationId,
+            organizationsAndRecruitersRelationTable.recruiterId,
+          ],
+        });
+      }
 
       await tx.insert(userTable).values({
         ...userWithoutProps,
