c5e51b2edcf18da49ecd6787a65963f9b63efcca
Jumpei Ogawa <git@phanective.org>
2025-08-13 22:35:03 +0900

    fix: remove link to /employer/jobs from candidates' navbar

--- src/routes/(public)/+layout.server.ts
+++ src/routes/(public)/+layout.server.ts
@@ -2,11 +2,11 @@ import { getSession } from "$lib/auth/server.ts";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ request }) => {
-  const session = await getSession({
+  const { user } = await getSession({
     headers: request.headers,
-  });
+  }) ?? {};
 
   return {
-    isLoggedIn: !!session,
+    userType: user?.props.type ?? "guest" as const,
   };
 };
