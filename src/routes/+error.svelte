9839792f22cf6fa569d07fbf40bffe254f5838a1
Jumpei Ogawa <git@phanective.org>
2025-08-14 12:11:51 +0900

    fix: DB query error when the recruiter does not belong to any orgs

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+  import Link from "$lib/components/generic/Link.svelte";
   import { page } from "$app/state";
 </script>
 
-{#if page.status === 404}
+{#if page.error?.code === "RECRUITER_WITHOUT_ORG"}
+  <p>
+    You do not belong to any organization.
+    <Link href="/employer/orgs/new" theme="linktext" preload={true}>
+      Create a new organization
+    </Link> or ask your organization owner to add you.
+    <!-- TODO Add button to ask invitation to the org in the organization page -->
+  </p>
+{:else if page.status === 404}
   <h1>This page is not found.</h1>
 {:else}
   <h1>{ page.status }: { page.error?.message }</h1>
