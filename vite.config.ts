79259008213e051af3427d7cc5e6e1cc8baa8e41
Jumpei Ogawa <git@phanective.org>
2025-08-16 17:59:26 +0900

    chore: remove TailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import tailwindcss from "@tailwindcss/vite";
 import devtoolsJson from "vite-plugin-devtools-json";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     sveltekit(),
-    tailwindcss(),
     devtoolsJson(),
   ],
   server: {
