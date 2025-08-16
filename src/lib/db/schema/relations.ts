674eaa035962dab6fa21c17c078e1f51269d50f2
Jumpei Ogawa <git@phanective.org>
2025-08-16 13:11:00 +0900

    refactor: use plural for `jobsAndUsersRelationTable`

--- src/lib/db/schema/relations.ts
+++ src/lib/db/schema/relations.ts
@@ -7,7 +7,7 @@ import {
 import { job } from "./job.ts";
 import { user } from "./better-auth.ts";
 
-export const jobAndUserRelationTable = pgTable("jobsAndCandidatesRelation", {
+export const jobsAndUsersRelationTable = pgTable("jobsAndCandidatesRelation", {
   appliedJobId: uuid().notNull().references(() => job.id),
   candidateId: text().notNull().references(() => user.id),
 }, (t) => [ primaryKey({ columns: [ t.appliedJobId, t.candidateId ]}) ]);
