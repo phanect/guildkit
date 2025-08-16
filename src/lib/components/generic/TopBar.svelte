5dac91b5107e9abd3408647ae01d8865cf31c641
Jumpei Ogawa <git@phanective.org>
2025-08-16 17:42:10 +0900

    refactor: extract `<TopBar>` as a component

---
+++ src/lib/components/generic/TopBar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  type Props = {
+    children: Snippet;
+  };
+
+  const { children }: Props = $props();
+</script>
+
+<div style="display: flex; justify-content: center; align-items: center; width=100%; height: 2.5rem; background-color: red; color: white;">
+  {@render children()}
+</div>
