22235ff146449267726fa663fdf84eda2ede8fea
Jumpei Ogawa <git@phanective.org>
2025-08-17 20:32:54 +0900

    feat: add deep color button theme

--- src/lib/components/generic/Button.svelte
+++ src/lib/components/generic/Button.svelte
@@ -5,11 +5,11 @@
   type Props = {
     children: Snippet;
     onclick?: MouseEventHandler<HTMLButtonElement>;
-    theme?: "button" | "linktext";
+    theme: "button-deep" | "button-pale" | "linktext";
     width?: number | string;
   };
 
-  const { onclick, theme = "button", width, children }: Props = $props();
+  const { onclick, theme, width, children }: Props = $props();
 </script>
 
 <style lang="scss">
