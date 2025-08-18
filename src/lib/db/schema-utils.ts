f1f5bac88718915ed124ccd19a934d5a45326048
Jumpei Ogawa <git@phanective.org>
2025-08-18 12:57:08 +0900

    fix: change type of `orgProps.id` to `integer`
    BetterAuth does not allow `uuid` type.

--- src/lib/db/schema-utils.ts
+++ src/lib/db/schema-utils.ts
@@ -4,3 +4,8 @@ export const timeLogs = {
   createdAt: timestamp().defaultNow().notNull(),
   updatedAt: timestamp().$onUpdateFn(() => new Date()),
 };
+
+/**
+ * @returns A pseudo-random number less than 2,147,483,647 (Max number of integer)
+ */
+export const randomId = () => Math.floor(Math.random() * 2147483647);
