2cbb48ec81a3d953b92931a23de77223da8fe9b0
Jumpei Ogawa <git@phanective.org>
2025-08-22 19:56:58 +0900

    chore: remove ESLint configs which are now included in `@phanect/lint[-svelte]`

--- eslint.config.ts
+++ eslint.config.ts
@@ -27,29 +27,6 @@ const configs: Linter.Config[] = [
       },
     },
   },
-
-  // In this project, I use ESLint rather than tsc's `noUnusedLocals`
-  // to check unused imports because I have to ignore unused import warning
-  // in tmp/drizzle-schema/better-auth.
-  {
-    files: [ "**/*.js" ],
-    rules: {
-      "no-unused-vars": "error",
-    },
-  },
-  {
-    files: [ "**/*.ts" ],
-    rules: {
-      "@typescript-eslint/no-unused-vars": "error",
-    },
-  },
-  {
-    files: [ "**/*.svelte" ],
-    rules: {
-      // Maybe it does not work properly on Svelte?
-      "@typescript-eslint/no-unsafe-member-access": "off",
-    },
-  },
 ];
 
 export default configs;
