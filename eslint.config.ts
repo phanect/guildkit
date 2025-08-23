6db84901990863e5e12e134cb12c245420aed41c
Jumpei Ogawa <git@phanective.org>
2025-08-23 10:20:51 +0900

    chore: update ESLint config for Next.js

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,22 @@
 import { core } from "@phanect/lint";
-import { svelte } from "@phanect/lint-svelte";
-import svelteConfig from "./svelte.config.js";
+import { next } from "@phanect/lint-react";
 import type { Linter } from "eslint";
 
 const configs: Linter.Config[] = [
   {
     ignores: [
+      "./.next/**",
       "./build/**",
+      "./out/**",
       "./tmp/**",
       "./src/lib/db/schema/better-auth.ts",
       "./src/lib/db/schema/currencies.ts",
       "./src/lib/db/schema/index.ts",
+      "next-env.d.ts",
     ],
   },
   ...core,
-  ...svelte,
+  ...next,
 
   {
     // Do not add `files: [ "*" ],` here.
@@ -23,7 +25,6 @@ const configs: Linter.Config[] = [
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
-        svelteConfig,
       },
     },
   },
