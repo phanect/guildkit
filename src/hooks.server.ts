28282a1e3ab0113cbe03a80763ba99bd4b023063
Jumpei Ogawa <git@phanective.org>
2025-08-23 12:07:58 +0900

    refactor: migrate betterAuth from SvelteKit to Next.js

--- src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-import { svelteKitHandler } from "better-auth/svelte-kit";
-import { building } from "$app/environment";
-import { auth } from "$lib/auth.ts";
-import type { Handle } from "@sveltejs/kit";
-
-export const handle: Handle = async ({ event, resolve }) =>
-  svelteKitHandler({ event, resolve, auth, building }) as Promise<Response>;
