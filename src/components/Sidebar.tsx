67b3fa54e51b87ae3b4de590efd885fff6ae5b2a
Jumpei Ogawa <git@phanective.org>
2025-08-25 19:45:48 +0900

    refactor: Reactify navigation components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,101 +1,53 @@
-<script lang="ts">
-  import { signOut } from "$lib/auth/client.ts";
-</script>
+"use client";
 
-<style lang="scss">
-  .sidebar {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+import Image from "next/image";
+import { signOut } from "@/lib/auth/client.ts";
+import type { ReactElement } from "react";
 
-    width: fit-content;
-    height: 100%;
+export const Sidebar = (): ReactElement => (
+  <aside className="flex flex-col items-center w-fit h-full bg-white text-gray-600 shadow-[#e0e0e0_1.25rem_0_1rem_-1rem] sm:flex hidden">
+    <ul className="w-full">
+      <li className="flex justify-center items-center w-full h-16 px-4 py-2 hover:bg-pink-50 hover:w-full">
+        <a href="." className="w-fit h-fit cursor-pointer">
+          <Image src="/vendor/tabler/inbox.svg" alt="Inbox" width={32} height={32} />
+        </a>
+      </li>
 
-    background-color: #ffffff;
-    color: #414141;
-    box-shadow: #e0e0e0 1.25rem 0 1rem -1rem;
-  }
+      <li className="flex justify-center items-center w-full h-16 px-4 py-2 hover:bg-pink-50 hover:w-full">
+        <a href="." className="w-fit h-fit cursor-pointer">
+          <Image src="/vendor/tabler/bookmark.svg" alt="Bookmarks" width={32} height={32} />
+        </a>
+      </li>
 
-  .fit {
-    width: fit-content;
-    height: fit-content;
+      <li className="flex justify-center items-center w-full h-16 px-4 py-2 hover:bg-pink-50 hover:w-full">
+        <a href="." className="w-fit h-fit cursor-pointer">
+          <Image src="/vendor/tabler/trash.svg" alt="Trash" width={32} height={32} />
+        </a>
+      </li>
 
-    // For `<button>`
-    cursor: pointer;
-  }
+      <li className="flex justify-center items-center w-full h-16 px-4 py-2 hover:bg-pink-50 hover:w-full">
+        <a href="." className="w-fit h-fit cursor-pointer">
+          <Image src="/vendor/tabler/shopping-cart.svg" alt="Cart" width={32} height={32} />
+        </a>
+      </li>
 
-  .list {
-    width: 100%;
-  }
-  .item {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+      <li className="flex justify-center items-center w-full h-16 px-4 py-2 hover:bg-pink-50 hover:w-full">
+        <a href="." className="w-fit h-fit cursor-pointer">
+          <Image src="/vendor/tabler/settings.svg" alt="Settings" width={32} height={32} />
+        </a>
+      </li>
 
-    width: 100%;
-    height: 4rem;
+      <li className="flex justify-center items-center w-full h-16 px-4 py-2 hover:bg-pink-50 hover:w-full">
+        <a href="." className="w-fit h-fit cursor-pointer">
+          <Image src="/vendor/tabler/bell.svg" alt="Notifications" width={32} height={32} />
+        </a>
+      </li>
 
-    padding-left: 1rem;
-    padding-right: 1rem;
-    padding-top: 0.5rem;
-    padding-bottom: 0.5rem;
-
-    &:hover {
-      background-color: #f6f3f4;
-      width: 100%;
-    }
-  }
-
-  @media (max-width: 640px) {
-    .sidebar {
-      // TODO Consider how to show this sidebar in mobile view
-      display: none;
-    }
-  }
-</style>
-
-<aside class="sidebar">
-  <ul class="list">
-    <li class="item">
-      <a href="." class="fit">
-        <img src="/vendor/tabler/inbox.svg" alt="Inbox" width="32" height="32" decoding="async" />
-      </a>
-    </li>
-
-    <li class="item">
-      <a href="." class="fit">
-        <img src="/vendor/tabler/bookmark.svg" alt="Bookmarks" width="32" height="32" decoding="async" />
-      </a>
-    </li>
-
-    <li class="item">
-      <a href="." class="fit">
-        <img src="/vendor/tabler/trash.svg" alt="Trash" width="32" height="32" decoding="async" />
-      </a>
-    </li>
-
-    <li class="item">
-      <a href="." class="fit">
-        <img src="/vendor/tabler/shopping-cart.svg" alt="Cart" width="32" height="32" decoding="async" />
-      </a>
-    </li>
-
-    <li class="item">
-      <a href="." class="fit">
-        <img src="/vendor/tabler/settings.svg" alt="Settings" width="32" height="32" decoding="async" />
-      </a>
-    </li>
-
-    <li class="item">
-      <a href="." class="fit">
-        <img src="/vendor/tabler/bell.svg" alt="Notifications" width="32" height="32" decoding="async" />
-      </a>
-    </li>
-
-    <li class="item">
-      <button class="fit" onclick={ () => signOut() }>
-        <img src="/vendor/tabler/logout-2.svg" alt="Logout" width="32" height="32" decoding="async" />
-      </button>
-    </li>
-  </ul>
-</aside>
+      <li className="flex justify-center items-center w-full h-16 px-4 py-2 hover:bg-pink-50 hover:w-full">
+        <button className="w-fit h-fit cursor-pointer" onClick={() => void signOut()}>
+          <Image src="/vendor/tabler/logout-2.svg" alt="Logout" width={32} height={32} />
+        </button>
+      </li>
+    </ul>
+  </aside>
+);
