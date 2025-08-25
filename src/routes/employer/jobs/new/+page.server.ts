1f83bb8291c21832cee66ac22a65a34e9b796c2e
Jumpei Ogawa <git@phanective.org>
2025-08-25 19:33:22 +0900

    refactor: Nextify /employer/jobs/new

--- src/routes/employer/jobs/new/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { superValidate } from "sveltekit-superforms/server";
-import { jobSchema } from "$lib/validation/job.validation.ts";
-import type { RequestEvent } from "@sveltejs/kit";
-
-export const load = async (event: RequestEvent) => {
-  const form = await superValidate(event, jobSchema);
-  return { form };
-};
