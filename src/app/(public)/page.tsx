4335d0b5aa8d3a0cf2580a1c54bf7c4138bee516
Jumpei Ogawa <git@phanective.org>
2025-08-25 19:25:07 +0900

    refactor: Nextify /

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,28 @@
-<script lang="ts">
-  import JobList from "$lib/components/JobList.svelte";
-  import type { PageProps } from "./$types";
+import { JobList } from "@/components/JobList.tsx";
+import { db } from "@/lib/db/db.ts";
 
-  const { data }: PageProps = $props();
-</script>
+export default async function Index() {
+  const today = new Date();
+  const jobs = await db.query.job.findMany({
+    columns: {
+      id: true,
+      title: true,
+      description: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+    with: {
+      employer: {
+        columns: {
+          name: true,
+        },
+      },
+    },
+    where: (job, { gte }) => gte(job.expiresAt, today),
+    orderBy: (job, { desc }) => [ desc(job.updatedAt) ],
+  });
 
-<JobList jobs={ data.jobs } />
+  return (
+    <JobList jobs={jobs} />
+  );
+}
