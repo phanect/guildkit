9a3dcb818a5ff66290c3569105e68b5fcf6c3ae3
Jumpei Ogawa <git@phanective.org>
2025-08-25 19:13:23 +0900

    refactor: Nextify /auth/error

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,15 +1,24 @@
-import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
+import { redirect } from "next/navigation";
+import type { ReactElement } from "react";
 
-export const load: PageServerLoad = async ({ url }) => {
-  const errorId = url.searchParams.get("error");
+type PageProps = {
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
+};
+
+export default async function AuthErrorPage({ searchParams }: PageProps): Promise<ReactElement> {
+  const { error: errorId } = await searchParams;
 
   if (errorId === "signup_disabled") {
-    return redirect(307, "/auth/signup");
+    redirect("/auth/signup");
   }
 
-  return {
-    message: errorId === "unable_to_create_user" ? "Unable to create user. We're sorry, this is probably our bug."
-    : "Unexpected error. We're sorry, this is probably our bug.",
-  };
-};
+  const message = errorId === "unable_to_create_user"
+    ? "Unable to create user. We're sorry, this is probably our bug."
+    : "Unexpected error. We're sorry, this is probably our bug.";
+
+  return (
+    <p>
+      {message}
+    </p>
+  );
+}
--- src/routes/auth/error/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import type { PageProps } from "./$types.d.ts";
-
-  const { data }: PageProps = $props();
-</script>
-
-<p>
-  { data.message }
-</p>
