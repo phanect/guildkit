5eb706ff8184dda585157336646121b033d04170
Jumpei Ogawa <git@phanective.org>
2025-08-25 19:28:23 +0900

    refactor: Nextify /jobs/[id]

--- src/app/(public)/jobs/[id]/page.tsx
+++ src/app/(public)/jobs/[id]/page.tsx
@@ -1,143 +1,111 @@
-<script lang="ts">
-  import Link from "$lib/components/generic/Link.svelte";
-  import { parseString } from "$lib/helpers/parseString.ts";
-  import type { PageProps } from "./$types";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { Link } from "@/components/generic/ButtonLink.tsx";
+import { db } from "@/lib/db/db.ts";
+import { parseString } from "@/lib/helpers/parseString.ts";
+import type { ReactElement } from "react";
 
-  const { data }: PageProps = $props();
-  const { job } = data;
-</script>
+type Props = {
+  params: Promise<{
+    id: string;
+  }>;
+};
 
-<div class="root">
-  <h1 class="title">{job.title}</h1>
-  <a href={`/orgs/${ job.employer.slug }`} class="employer">
-    <img src="/vendor/octicons/organization.svg" alt="Employer" title="Employer" width="18" />
-    {job.employer.name}
-  </a>
-  <div>
-    <span class="condition condition-location">
-      <img src="/vendor/octicons/location.svg" alt="" width="16" />
-      Work location: {job.location}
-    </span>
-    <span class="condition condition-salary">
-      <img src="/vendor/tabler/coins.svg" alt="" width="16" />
-      <!-- TODO Use user's locale in `toLocaleString()` -->
-      Salary: {job.salary.toLocaleString("en-US")} {job.currency}/{job.salaryPer}
-    </span>
-  </div>
-  <div class="last-updated">
-    <img src="/vendor/octicons/clock.svg" alt="" width="16" />
-    <!-- TODO Use user's locale in `toLocaleString()` -->
-    Last updated at {(job.updatedAt ?? job.createdAt).toLocaleDateString("en-US")}
-  </div>
+export default async function JobPage({ params }: Props): Promise<ReactElement> {
+  const { id: jobId } = await params;
 
-  <section class="section">
-    <h3 class="section-title">Description</h3>
-    <p>
-      {job.description}
-    </p>
-  </section>
+  const job = await db.query.job.findFirst({
+    columns: {
+      id: true,
+      title: true,
+      description: true,
+      location: true,
+      salary: true,
+      salaryPer: true,
+      currency: true,
+      applicationUrl: true,
+      requirements: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+    with: {
+      employer: {
+        columns: {
+          slug: true,
+          name: true,
+        },
+      },
+    },
+    where: (job, { eq }) => eq(job.id, jobId),
+    orderBy: (job, { desc }) => [ desc(job.updatedAt) ],
+  });
 
-  <section class="section">
-    <h3 class="section-title">Requirements</h3>
-    <ul class="requirements">
-      {#each parseString(job.requirements) as requirement, index (index)}
-        {#if requirement}
-          <li>{requirement}</li>
-        {/if}
-      {/each}
-    </ul>
-  </section>
-
-  <section class="apply">
-    <Link href={job.applicationUrl} theme="button-deep" preload>
-      Apply
-    </Link>
-  </section>
-</div>
-
-<style lang="scss">
-  @use "$lib/styles/mixins.scss";
-
-  .root {
-    @include mixins.page-root;
+  if (!job) {
+    notFound();
   }
 
-  .title {
-    font-size: 1.875rem;
-    font-weight: 900;
-    color: #111827;
-    margin: {
-      bottom: 0.5rem;
-    }
-  }
+  return (
+    <div className="w-full max-w-4xl px-9">
+      <h1 className="text-3xl font-black text-gray-900 mb-2">{job.title}</h1>
+      <Link
+        href={`/orgs/${ job.employer.slug }`}
+        className="inline-flex items-center gap-2 mb-3 text-lg font-bold"
+      >
+        <Image
+          src="/vendor/octicons/organization.svg"
+          alt="Employer"
+          title="Employer"
+          width={18}
+          height={18}
+        />
+        {job.employer.name}
+      </Link>
+      <div className="mb-6">
+        <span className="inline-flex items-center gap-1.5 mr-2 py-2 px-4 text-sm font-medium text-gray-800 bg-gray-200 rounded-full">
+          <Image src="/vendor/octicons/location.svg" alt="" width={16} />
+          Work location: {job.location}
+        </span>
+        <span className="inline-flex items-center gap-1.5 mr-2 py-2 px-4 text-sm font-medium text-gray-800 bg-green-200 rounded-full">
+          <Image src="/vendor/tabler/coins.svg" alt="" width={16} />
+          {/* TODO Use user's locale in `toLocaleString()` */}
+          Salary: {job.salary.toLocaleString("en-US")} {job.currency}/{job.salaryPer}
+        </span>
+      </div>
+      <div className="inline-flex items-center gap-1.5 justify-end w-full mb-6 text-sm">
+        <Image src="/vendor/octicons/clock.svg" alt="" width={16} />
+        {/* TODO Use user's locale in `toLocaleString()` */}
+        Last updated at {(job.updatedAt ?? job.createdAt).toLocaleDateString("en-US")}
+      </div>
 
-  .employer {
-    display: inline-flex;
-    gap: 0.5rem;
-    margin: {
-      bottom: 0.75rem;
-    }
+      <section className="mb-5">
+        <h3 className="text-2xl font-bold text-gray-800 mb-2">Description</h3>
+        <p>
+          {job.description}
+        </p>
+      </section>
 
-    font-size: 1.125rem;
-    font-weight: 700;
-    color: #020202;
-  }
+      <section className="mb-5">
+        <h3 className="text-2xl font-bold text-gray-800 mb-2">Requirements</h3>
+        <ul className="list-disc ml-8">
+          {parseString(job.requirements).map((requirement, index) => {
+            if (requirement) {
+              return <li key={index}>{requirement}</li>;
+            }
+            return null;
+          })}
+        </ul>
+      </section>
 
-  .condition {
-    @include mixins.text-with-icon;
-
-    margin-right: 0.5rem;
-    padding: {
-      top: 0.5rem;
-      bottom: 0.5rem;
-      left: 1rem;
-      right: 1rem;
-    }
-
-    font-size: 0.875rem;
-    font-weight: 500;
-    color: #1f2937;
-    border-radius: 9999px;
-  }
-
-  .condition-location {
-    background-color: #e5e7eb;
-  }
-
-  .condition-salary {
-    background-color: #bbf7d0;
-  }
-
-  .last-updated {
-    @include mixins.text-with-icon;
-
-    width: 100%;
-    justify-content: end;
-    margin: {
-      bottom: 1.5rem;
-    }
-
-    font-size: 0.875rem;
-  }
-
-  .section {
-    margin: {
-      bottom: 1.25rem;
-    }
-  }
-
-  .section-title {
-    font-size: 1.5rem;
-    font-weight: 700;
-    color: #1f2937;
-  }
-
-  .requirements {
-    list-style-type: disc;
-    margin-left: 2rem;
-  }
-
-  .apply {
-    margin-top: 1.875rem;
-  }
-</style>
+      <section className="mt-8">
+        <Link
+          href={job.applicationUrl}
+          theme="button-deep"
+          className="px-6 py-3 font-semibold rounded-lg"
+          prefetch
+        >
+          Apply
+        </Link>
+      </section>
+    </div>
+  );
+}
