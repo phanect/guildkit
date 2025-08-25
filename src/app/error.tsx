535b8e2f5e760d9b0f528f47db51e5930359188b
Jumpei Ogawa <git@phanective.org>
2025-08-25 19:40:26 +0900

    refactor: Nextify error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,18 +1,21 @@
-<script lang="ts">
-  import Link from "$lib/components/generic/Link.svelte";
-  import { page } from "$app/state";
-</script>
+"use client"; // error.tsx must be a Client Component
 
-{#if page.error?.code === "RECRUITER_WITHOUT_ORG"}
-  <p>
-    You do not belong to any organization.
-    <Link href="/employer/orgs/new" theme="linktext" preload={true}>
-      Create a new organization
-    </Link> or ask your organization owner to add you.
-    <!-- TODO Add button to ask invitation to the org in the organization page -->
-  </p>
-{:else if page.status === 404}
-  <h1>This page is not found.</h1>
-{:else}
-  <h1>{ page.status }: { page.error?.message }</h1>
-{/if}
+import { Link } from "@/components/generic/ButtonLink.tsx";
+
+export default function Error({ error }: {
+  error: Error & { digest?: string; };
+}) {
+  return error.cause === "RECRUITER_WITHOUT_ORG" ? (
+    <p>
+      You do not belong to any organization.
+      <Link href="/employer/orgs/new" theme="linktext" prefetch>
+        Create a new organization
+      </Link> or ask your organization owner to add you.
+      {/* TODO Add button to ask invitation to the org in the organization page */}
+    </p>
+  ) : error.cause === 404 ? (
+    <h1>This page is not found.</h1>
+  ) : (
+    <h1>{ error.message }</h1>
+  );
+}
