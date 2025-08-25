77b42c2abd981ac9d6fd35bd6fe19f34e0146bf8
Jumpei Ogawa <git@phanective.org>
2025-08-25 19:40:56 +0900

    refactor: convert SvelteKit's app.html to Next.js's root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <!-- <link rel="icon" href="%sveltekit.assets%/images/logo.svg" /> -->
-    <link rel="icon" href="https://tmp.guildkit.net/canvaai/guildkit_icon_tmp.png" />
-    <meta name="viewport" content="width=device-width" />
-    %sveltekit.head%
-  </head>
-  <body data-sveltekit-preload-data="hover">
-    <div style="display: contents">%sveltekit.body%</div>
-  </body>
-</html>
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  // TODO Make these modifiable
+  title: "GuildKit",
+  description: "A toolkit for building job search services",
+};
+
+export default function RootLayout({ children }: Readonly<{ children: ReactNode; }>) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="https://tmp.guildkit.net/canvaai/guildkit_icon_tmp.png" />
+        <meta name="viewport" content="width=device-width" />
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
