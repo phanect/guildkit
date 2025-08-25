4b84f03e2921d5ce62df43d6dacd46cde4d686f8
Jumpei Ogawa <git@phanective.org>
2025-08-25 19:43:17 +0900

    refactor: Reactify `<CenterBox>` component

--- src/components/generic/CenterBox.tsx
+++ src/components/generic/CenterBox.tsx
@@ -1,32 +1,15 @@
-<script lang="ts">
-  import type { Snippet } from "svelte";
+import type { ReactElement, ReactNode } from "react";
 
-  type Props = {
-    children: Snippet;
-  };
+type Props = {
+  children: ReactNode;
+};
 
-  const { children }: Props = $props();
-</script>
-
-<style lang="scss">
-  .container-outer {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    width: 100%;
-    height: 100vh;
-  }
-
-  .container-inner {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-</style>
-
-<div class="container-outer">
-  <div class="container-inner">
-    {@render children()}
-  </div>
-</div>
+export function CenterBox({ children }: Props): ReactElement {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <div className="flex flex-col items-center">
+        {children}
+      </div>
+    </div>
+  );
+}
