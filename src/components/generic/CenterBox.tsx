8b06f44a285557ffa872abd1b88bf5b2ab8cc1b8
Jumpei Ogawa <git@phanective.org>
2025-08-31 14:45:44 +0900

    feat: give up vertical centering
    Vertical centering does not work if there is `<Nav>` in the page.

--- src/components/generic/CenterBox.tsx
+++ src/components/generic/CenterBox.tsx
@@ -6,10 +6,8 @@ type Props = {
 
 export function CenterBox({ children }: Props): ReactElement {
   return (
-    <div className="flex justify-center items-center w-full h-screen">
-      <div className="flex flex-col items-center">
-        {children}
-      </div>
+    <div className="flex flex-col items-center">
+      {children}
     </div>
   );
 }
