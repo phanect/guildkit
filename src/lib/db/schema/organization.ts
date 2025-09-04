68120c7b6fe7c9036af9677c7fe813eeb2bd2b5f
Jumpei Ogawa <git@phanective.org>
2025-09-04 16:58:07 +0900

    refactor: migrate `orgProps` table to betterAuth's `additionalFields`

--- src/lib/db/schema/organization.ts
+++ src/lib/db/schema/organization.ts
@@ -1,28 +1,9 @@
-import { relations, type InferSelectModel } from "drizzle-orm";
-import { pgTable, text, integer } from "drizzle-orm/pg-core";
-import { currency } from "./currencies.ts";
+import { relations } from "drizzle-orm";
 import { organization } from "./better-auth.ts";
-import { timeLogs, randomId } from "../schema-utils.ts";
 import { job } from "./job.ts";
 import { organizationsAndRecruitersRelationTable } from "./relations.ts";
 
-export const organizationRelations = relations(organization, ({ one, many }) => ({
-  props: one(orgProps, {
-    fields: [ organization.propsId ],
-    references: [ orgProps.id ],
-  }),
+export const organizationRelations = relations(organization, ({ many }) => ({
   recruiters: many(organizationsAndRecruitersRelationTable),
   jobs: many(job),
 }));
-
-export const orgProps = pgTable("orgProps", {
-  id: integer().primaryKey().notNull().$defaultFn(() => randomId()),
-  about: text(),
-  url: text().notNull().unique(),
-  emails: text().array(),
-  addresses: text().array().notNull(),
-  currencies: currency().array().notNull(),
-  ...timeLogs,
-});
-
-export type OrgProps = InferSelectModel<typeof orgProps>;
