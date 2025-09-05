651f22fa46feb7fdf617d95e82d05779bed66f61
Jumpei Ogawa <git@phanective.org>
2025-09-05 19:32:04 +0900

    chore: add currency name and country name to currency object

--- src/lib/db/schema/currencies.ts
+++ src/lib/db/schema/currencies.ts
@@ -2,4 +2,4 @@ import { pgEnum } from "drizzle-orm/pg-core";
 // Cannot use `@/intermediate` since this file is not managed by Next.js
 import { currencies } from "../../../intermediate/currencies.ts";
 
-export const currency = pgEnum("Currency", currencies);
+export const currency = pgEnum("Currency", currencies.map((currency) => currency.code) as [string, ...string[]]);
