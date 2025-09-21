943a84a7038453e535b12644f10c4e905996f571
Jumpei Ogawa <git@phanective.org>
2025-09-21 11:27:35 +0900

    chore: rewrite seed script with BetterAuth API

--- src/lib/db/helpers.ts
+++ src/lib/db/helpers.ts
@@ -1,44 +1,8 @@
 import "dotenv/config";
 import { eq, type InferInsertModel } from "drizzle-orm";
 import { db } from "./db.ts";
-import { user as userTable } from "./schema/better-auth.ts";
 import { userProps } from "./schema/user.ts";
-import { organizationsAndRecruitersRelationTable } from "./schema/relations.ts";
-
-export type UserWithProps = Omit<InferInsertModel<typeof userTable>, "propsId" | "recruitsFor"> & {
-  props: InferInsertModel<typeof userProps>;
-  recruitsFor?: string[];
-};
-
-export const insertUsers = async (users: UserWithProps | UserWithProps[]) => {
-  const usersWithProps = Array.isArray(users) ? users : [ users ];
-
-  // TODO Fix N+1 problem
-  await db.transaction(async (tx) => {
-    for (const userWithProps of usersWithProps) {
-      const { props, recruitsFor: orgIds = [], ...userWithoutProps } = userWithProps;
-
-      const [{ propsId }] = await tx.insert(userProps).values(props).returning({ propsId: userProps.id });
-
-      if (0 < orgIds.length) {
-        tx.insert(organizationsAndRecruitersRelationTable).values(orgIds.map((orgId) => ({
-          organizationId: orgId,
-          recruiterId: userWithoutProps.id,
-        }))).onConflictDoNothing({
-          target: [
-            organizationsAndRecruitersRelationTable.organizationId,
-            organizationsAndRecruitersRelationTable.recruiterId,
-          ],
-        });
-      }
-
-      await tx.insert(userTable).values({
-        ...userWithoutProps,
-        propsId: propsId,
-      });
-    }
-  });
-};
+import type { user as userTable } from "./schema/better-auth.ts";
 
 export const updateUserProps = (user: InferInsertModel<typeof userTable>) => {
   type UpdateFn = typeof db.update<typeof userProps>;
