d1a3a2faa8e14cea44169e5feca3b61e1e61f239
Jumpei Ogawa <git@phanective.org>
2025-09-22 16:10:51 +0900

    feat: set active organization on login & `requireAuthAs()`

--- src/lib/auth/types.ts
+++ src/lib/auth/types.ts
@@ -6,3 +6,5 @@ export type User = typeof auth.$Infer.Session["user"] & {
 };
 
 export type Session = typeof auth.$Infer.Session["session"];
+
+export type Organization = typeof auth.$Infer.Organization;
