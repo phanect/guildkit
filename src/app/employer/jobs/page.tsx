fade1230fd3a7a6ee5acb088b60360023eeae17b
Jumpei Ogawa <git@phanective.org>
2025-09-24 17:07:15 +0900

    fix: process `RECRUITER_WITHOUT_ORGS` in the layout of /employer/*

--- src/app/employer/jobs/page.tsx
+++ src/app/employer/jobs/page.tsx
@@ -3,40 +3,49 @@ import { JobList } from "@/components/JobList.tsx";
 import { requireAuthAs } from "@/lib/auth/server.ts";
 import { db } from "@/lib/db/db.ts";
 import type { JobCardInfo } from "@/components/JobCard.tsx";
+import { GuildKitError } from "@/lib/utils/errors.ts";
 
 export default async function EmployerJobsPage() {
-  const { user, session } = await requireAuthAs("recruiter");
+  try {
+    const { user, session } = await requireAuthAs("recruiter");
 
-  const jobs: JobCardInfo[] = await db.query.job.findMany({
-    columns: {
-      id: true,
-      title: true,
-      description: true,
-      createdAt: true,
-      updatedAt: true,
-    },
-    with: {
-      employer: {
-        columns: {
-          name: true,
+    const jobs: JobCardInfo[] = await db.query.job.findMany({
+      columns: {
+        id: true,
+        title: true,
+        description: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+      with: {
+        employer: {
+          columns: {
+            name: true,
+          },
         },
       },
-    },
-    where: (job, { eq }) => eq(job.employer, session.activeOrganizationId),
-    orderBy: (job, { desc }) => [ desc(job.updatedAt) ],
-  });
+      where: (job, { eq }) => eq(job.employer, session.activeOrganizationId),
+      orderBy: (job, { desc }) => [ desc(job.updatedAt) ],
+    });
 
-  const editable = user.props.type === "recruiter";
+    const editable = user.props.type === "recruiter";
 
-  return (
-    <div className="flex flex-col items-center gap-y-10 w-full">
-      <section className="flex justify-start bg-gray-100 shadow-lg rounded-lg w-[42.5rem] max-w-full p-4">
-        <Link href="/employer/jobs/new" theme="button-deep" prefetch>
-          Add job
-        </Link>
-      </section>
+    return (
+      <div className="flex flex-col items-center gap-y-10 w-full">
+        <section className="flex justify-start bg-gray-100 shadow-lg rounded-lg w-[42.5rem] max-w-full p-4">
+          <Link href="/employer/jobs/new" theme="button-deep" prefetch>
+            Add job
+          </Link>
+        </section>
 
-      <JobList jobs={jobs} editable={editable} />
-    </div>
-  );
+        <JobList jobs={jobs} editable={editable} />
+      </div>
+    );
+  } catch (err) {
+    if (err instanceof GuildKitError && err.code === "RECRUITER_WITHOUT_ORGS") {
+      return (<></>); // The error would be processed in the layout
+    } else {
+      throw err;
+    }
+  }
 };
--- src/app/employer/layout.tsx
+++ src/app/employer/layout.tsx
@@ -1,6 +1,9 @@
+import { Link } from "@/components/generic/ButtonLink.tsx";
+import { CenterBox } from "@/components/generic/CenterBox.tsx";
 import { Nav } from "@/components/Nav.tsx";
 import { Sidebar } from "@/components/Sidebar.tsx";
 import { requireAuthAs } from "@/lib/auth/server.ts";
+import { GuildKitError } from "@/lib/utils/errors.ts";
 import type { ReactElement, ReactNode } from "react";
 
 type Props = {
@@ -8,17 +11,38 @@ type Props = {
 };
 
 export default async function EmployerLayout({ children }: Props): Promise<ReactElement> {
-  const { user } = await requireAuthAs("recruiter");
+  try {
+    const { user } = await requireAuthAs("recruiter");
 
-  return (
-    <>
-      <Nav for={user.props.type ?? "guest"} />
-      <div className="flex justify-center w-full">
-        <Sidebar />
-        <main className="flex flex-col items-center gap-4 w-full">
-          {children}
-        </main>
-      </div>
-    </>
-  );
+    return (
+      <>
+        <Nav for={user.props.type ?? "guest"} />
+        <div className="flex justify-center w-full">
+          <Sidebar />
+          <main className="flex flex-col items-center gap-4 w-full">
+            {children}
+          </main>
+        </div>
+      </>
+    );
+  } catch (err) {
+    if (err instanceof GuildKitError && err.code === "RECRUITER_WITHOUT_ORGS") {
+      return (
+        <>
+          <Nav for="guest" />
+          <CenterBox>
+            <p>
+              You do not belong to any organization.<br />
+              <Link href="/orgs/new" theme="linktext" prefetch>
+                Create a new organization
+              </Link> or ask your organization owner to add you.
+              {/* TODO Add button to ask invitation to the org in the organization page */}
+            </p>
+          </CenterBox>
+        </>
+      );
+    } else {
+      throw err;
+    }
+  }
 }
