7d36d0c8ce845d24b5d385f2b6537d8f62b11213
Jumpei Ogawa <git@phanective.org>
2025-10-10 12:15:47 +0900

    wip: feat: add `salaryPer` field to create job form

--- src/lib/actions/jobs.ts
+++ src/lib/actions/jobs.ts
@@ -22,6 +22,7 @@ export const createJob = async (_initialState: ActionState<Job>, formData: FormD
     applicationUrl: formData.get("applicationUrl"),
     location: formData.get("location"),
     salary: formData.get("salary"),
+    salaryPer: formData.get("salaryPer"),
     currency: formData.get("currency"),
     expiresAt: formData.get("expiresAt"),
   });
