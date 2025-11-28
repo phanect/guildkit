fced9d874d0488c9658740ab8be92753a760b325
Jumpei Ogawa <git@phanective.org>
2025-11-28 11:21:35 +0900

    refactor: migrate from Min.io to LocalStack
    Min.io is no longer maintained.

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -11,25 +11,22 @@ if (storagePlatform === "cloudflare" && !cloudflareAccountId) {
   throw new Error("Cloudflare account ID is not configured for Cloudflare R2. Set `accountId` in your guildkit.config.ts or `CLOUDFLARE_ACCOUNT_ID` environment variable.");
 }
 
-const s3Config: S3ClientConfig = {
-  ...storageConfig,
-
-  // Hard-coded configs
-  ...(
-    storagePlatform === "development" ? {
-      endpoint: "http://localhost:9000",
-      forcePathStyle: true, // Required for Min.io
-      region: "us-east-1", // Min.io's default
-      credentials: {
-        accessKeyId: "guildkit", // Same as MINIO_ROOT_USER configured in compose.yaml
-        secretAccessKey: "guildkit", // Same as MINIO_ROOT_PASSWORD configured in compose.yaml
-      },
-    } : storagePlatform === "cloudflare" ? {
-      endpoint: config.storage.endpoint ?? `https://${ cloudflareAccountId }.r2.cloudflarestorage.com`,
-      region: "auto", // Cloudflare's default
-    } : {} // empty if storagePlatform === "aws" or "custom"
-  ),
-};
+const s3Config: S3ClientConfig = process.env.SERVER_ENV === "development"
+  ? {
+    endpoint: "http://localhost:4566",
+    forcePathStyle: true,
+    region: "us-east-1",
+    credentials: {
+      // See: https://docs.localstack.cloud/aws/capabilities/config/credentials/
+      accessKeyId: "test",
+      secretAccessKey: "test",
+    },
+  }
+  : process.env.SERVER_ENV === "production" ? {
+    endpoint: `https://${ process.env.CLOUDFLARE_ACCOUNT_ID }.r2.cloudflarestorage.com`,
+    region: "auto", // Cloudflare's default
+  }
+  : {}; // empty if storagePlatform === "aws" or "custom"
 
 export const storage = new S3Client(s3Config);
 
