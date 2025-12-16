4fa7945ae3a556e7f7f5568ea3b760029cdde417
Jumpei Ogawa <git@phanective.org>
2025-12-16 09:40:03 +0900

    refactor: migrate to Prisma ― schema

--- src/lib/db/schema/currencies.ts
+++
@@ -1,5 +0,0 @@
-import { pgEnum } from "drizzle-orm/pg-core";
-// Cannot use `@/intermediate` since this file is not managed by Next.js
-import { currencies } from "../../../intermediate/currencies.ts";
-
-export const currency = pgEnum("Currency", currencies);
