5883e6a10ea22c8ca15b635bbfccebcb85fa4f8d
Jumpei Ogawa <git@phanective.org>
2025-12-16 09:40:03 +0900

    refactor: migrate to Prisma ― schema

--- src/lib/db/schema-utils.ts
+++
@@ -1,6 +0,0 @@
-import { timestamp } from "drizzle-orm/pg-core";
-
-export const timeLogs = {
-  createdAt: timestamp().defaultNow().notNull(),
-  updatedAt: timestamp().$onUpdateFn(() => new Date()),
-};
