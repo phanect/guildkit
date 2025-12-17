6443c1ac4be47ab09698b15652be499502a3a3fe
Jumpei Ogawa <git@phanective.org>
2025-12-17 13:57:23 +0900

    refactor: extract `<TopBar>` from `<Nav>` to root layout

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,6 @@
 import { headers } from "next/headers";
 import Image from "next/image";
 import { Link } from "@/components/generic/ButtonLink.tsx";
-import { TopBar } from "@/components/generic/TopBar.tsx";
 import { SignOutButton } from "@/components/SignOutButton.tsx";
 import { auth } from "@/lib/auth.ts";
 import config from "../../guildkit.config.ts";
@@ -22,50 +21,42 @@ export const Nav = async ({ for: userType }: Props): Promise<ReactElement> => {
   });
 
   return (
-    <>
-      {/* ▼▼ TODO Pre-alpha caution: Delete on the official release ▼▼ */}
-      <TopBar>
-        Caution: GuildKit is still pre-alpha state and there are probably a lot of bugs. Do not enter any private information for your security.
-      </TopBar>
-      {/* ▲▲ Pre-alpha caution ▲▲ */}
-
-      <nav className="flex items-center justify-between flex-wrap py-6 px-20">
-        <Link href="/" className="flex items-center gap-3 text-gray-900 text-xl font-semibold">
-          <Image
-            src="/logo.png"
-            width={64}
-            height={64}
-            alt=""
-            decoding="async"
-          />
-          <span>{ config.siteName }</span>
-        </Link>
-        <div className="flex items-center gap-4">
-          { isRecruiter && (
-            activeOrg ? (
-              <>
-                <Link href="/employer/jobs" theme="none" className="mr-8 font-bold">
-                  Dashboard
-                </Link>
-                <Link href={`/orgs/${ activeOrg.slug }`}>
-                  {activeOrg.name}
-                </Link>
-              </>
-            ) : (
-              <p>
-                <Link href="/orgs/new" theme="linktext">Create your company</Link><br />
-                or ask your boss to invite
-              </p>
-            )
-          )}
-
-          {userType === "guest" ? (
-            <Link href="/auth" theme="button-deep">Log in <span className="after:content-['|'] after:text-gray-500"></span> Sign up</Link>
+    <nav className="flex items-center justify-between flex-wrap py-6 px-20">
+      <Link href="/" className="flex items-center gap-3 text-gray-900 text-xl font-semibold">
+        <Image
+          src="/logo.png"
+          width={64}
+          height={64}
+          alt=""
+          decoding="async"
+        />
+        <span>{ config.siteName }</span>
+      </Link>
+      <div className="flex items-center gap-4">
+        { isRecruiter && (
+          activeOrg ? (
+            <>
+              <Link href="/employer/jobs" theme="none" className="mr-8 font-bold">
+                Dashboard
+              </Link>
+              <Link href={`/orgs/${ activeOrg.slug }`}>
+                {activeOrg.name}
+              </Link>
+            </>
           ) : (
-            <SignOutButton />
-          )}
-        </div>
-      </nav>
-    </>
+            <p>
+              <Link href="/orgs/new" theme="linktext">Create your company</Link><br />
+              or ask your boss to invite
+            </p>
+          )
+        )}
+
+        {userType === "guest" ? (
+          <Link href="/auth" theme="button-deep">Log in <span className="after:content-['|'] after:text-gray-500"></span> Sign up</Link>
+        ) : (
+          <SignOutButton />
+        )}
+      </div>
+    </nav>
   );
 };
