6a29c0a0e30d9528babb020f9eb9fe69a0efacc3
Jumpei Ogawa <git@phanective.org>
2026-03-12 21:17:42 +0900

    feat: custom site name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import "@/lib/styles/globals.css";
+import config from "../../guildkit.config.ts";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
-  // TODO Make these modifiable
-  title: "GuildKit",
+  title: config.siteName,
+  // TODO Make this modifiable
   description: "A toolkit for building job search services",
 };
 
