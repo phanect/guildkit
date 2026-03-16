385776b033e07329c4f93465511fde313d0ee62b
Jumpei Ogawa <git@phanective.org>
2026-03-16 09:38:42 +0900

    Revert "refactor: migrate to Prisma ― seed"
    This reverts commit f41634daf4d409c7981a740976447bdc8d29ad3b.

--- .mise/tasks/seed.ts
+++ .mise/tasks/seed.ts
@@ -1,10 +1,17 @@
 #!/usr/bin/env -S pnpm exec jiti
 
-import { randomUUID } from "node:crypto";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient, type User } from "../../src/lib/prisma/client.ts";
+import dayjs from "dayjs";
+import { db } from "../../src/lib/db/db.ts";
+import {
+  insertOrganizations,
+  insertUsers,
+  type OrgWithRecruiters,
+  type UserWithProps,
+} from "../../src/lib/db/helpers.ts";
+import { job } from "../../src/lib/db/schema/job.ts";
+import type { InferInsertModel } from "drizzle-orm";
 
-const candidates: User[] = [
+const candidates: UserWithProps[] = [
   {
     id: "heizou",
     name: "Heizou Shikanoin",
@@ -37,7 +44,7 @@ const candidates: User[] = [
   },
 ];
 
-const recruiterYae: User = {
+const recruiterYae = {
   id: "yae",
   name: "Miko Yae",
   email: "miko.yae@yaedo.example.co.jp",
@@ -46,8 +53,8 @@ const recruiterYae: User = {
   props: {
     type: "recruiter",
   },
-} as const;
-const recruiterRaiden: User = {
+} as const satisfies UserWithProps;
+const recruiterRaiden = {
   id: "ei",
   name: "Ei Raiden",
   email: "ei.raiden@shogunate.example.go.jp",
@@ -56,8 +63,8 @@ const recruiterRaiden: User = {
   props: {
     type: "recruiter",
   },
-} as const;
-const recruiterHiiragi: User = {
+} as const satisfies UserWithProps;
+const recruiterHiiragi = {
   id: "chisato",
   name: "Chisato Hiiragi",
   email: "chisato.hiiragi@shogunate.example.go.jp",
@@ -66,8 +73,8 @@ const recruiterHiiragi: User = {
   props: {
     type: "recruiter",
   },
-} as const;
-const recruiterZhongli: User = {
+} as const satisfies UserWithProps;
+const recruiterZhongli = {
   id: "zhongli",
   name: "Zhongli",
   email: "zhongli@wangsheng.example.com",
@@ -76,85 +83,228 @@ const recruiterZhongli: User = {
   props: {
     type: "recruiter",
   },
-} as const;
+} as const satisfies UserWithProps;
 
-const seed = async () => {
-  if (process.env.SERVER_ENV !== "development") {
-    console.info("Seeding are only allowed when SERVER_ENV is development. Seeding skipped.");
+const initialOrgYaedo: OrgWithRecruiters = {
+  id: "yph",
+  slug: "yaedo",
+  name: "Yae Publishing House, K.K.",
+  about: "Yae Publishing House is a leading light novel publisher from Inazuma. We also provide digital platforms to deliver the light novels and the other books to the global audience all over the Teyvat.",
+  url: "https://yaedo.example.co.jp",
+  addresses: [ "2-14-3, Hanamizaka, Inazuma City, Narukami Island, Inazuma" ],
+  emails: [ "hr@yaedo.example.co.jp" ],
+  currencies: [ "JPY" ],
+  createdAt: new Date(),
+  recruiters: [ recruiterYae ],
+};
+
+const initialOrgShogunate: OrgWithRecruiters = {
+  id: "kanjou",
+  slug: "kanjou",
+  name: "Kanjou Commission, The Shogunate of Inazuma",
+  about: "Kanjou Commission is a part of the Tri-Commission, The Shogunate of Inazuma. We are responsible for the Shogunate's financial and international affairs.",
+  url: "https://shogunate.example.go.jp",
+  addresses: [
+    "The Inazuma Castle, 1-1-1, Inazuma City, Narukami Island, Inazuma",
+    "Tenryou Commission Office, 1-2-5, Inazuma City, Narukami Island, Inazuma",
+    "The Hiiragi Estate, 5-1-1, Rito, Narukami Island, Inazuma",
+  ],
+  emails: [ "personnel@kanjou.example.go.jp" ],
+  currencies: [ "JPY" ],
+  createdAt: new Date(),
+  recruiters: [ recruiterRaiden, recruiterHiiragi ],
+};
+
+const initialOrgWangsheng: OrgWithRecruiters = {
+  id: "wangsheng",
+  slug: "wangsheng",
+  name: "Wangsheng Funeral Parlor",
+  url: "https://wangsheng.example.com",
+  addresses: [
+    "123456 Feiyun Slope, Liyue Harbor, Liyue",
+  ],
+  emails: [ "hutao@wangsheng.example.com" ],
+  currencies: [ "CNY" ],
+  createdAt: new Date(),
+  recruiters: [ recruiterZhongli ],
+  // Lazy Hu Tao didn't write `about` ― to test layout when about is not written.
+};
+
+const initialJobs: InferInsertModel<typeof job>[] = [
+  {
+    title: "[WFH] TypeScript Developer for our ebook store (Svelte / Hono / React Native)",
+    description: `
+      We are hiring a software developer for our ebook store. Your will be responsibile for one or some of the frontend, backend, and mobile app development.
+
+      # Responsibilities
+
+      You will be responsible for the one or more of the followings:
+
+      - Web frontend development for our ebook store (Svelte)
+      - Backend development for our ebook store (Hono + Cloudflare Workers)
+      - Mobile app development for our ebook store (React Native)
+
+      # Requirements
+
+      - One year or more experience in TypeScript development
+      - Two years or more experience in JavaScript or altJS (e.g. TypeScript, Elm, CoffeeScript)
+      - One year or more experience in one of the followings:
+        - Svelte, React, Vue.js, Angular, or any other component-oriented frontend framework(s)
+        - Node.js, Cloudflare Workers, or any other backend JavaScript/TypeScript framework(s)/platform(s)
+        - React Native or any other mobile JavaScript/TypeScript framework(s)
+    `.trim(),
+    applicationUrl: "https://phanective.org/job-example-1",
+    location: "Remote (any location in Inazuma)",
+    salary: 8000000,
+    currency: "JPY",
+    salaryPer: "YEAR",
+    expiresAt: dayjs().add(1, "month").toDate(),
+    employer: initialOrgYaedo.id,
+  },
+  {
+    title: "[WFH] SRE for our ebook store",
+    description: `
+      We are hiring an SRE for our ebook store.
+
+      # Responsibilities
+
+      You will be responsible for the infrastructure administration and the site reliability engineering for our online ebook store.
+
+      # Requirements
+
+      ## Required
+
+      - Two year or more experience as an SRE or an infrastructure engineer.
+      - One year or more experience in the production Docker server administration
+      - One year or more experience in Infrastructure as Code tools such as Terraform and Ansible
+
+      ## Nice to have
+
+      - Experience in Node.js server administration
+    `.trim(),
+    applicationUrl: "https://phanective.org/job-example-2",
+    location: "Remote (any location in Inazuma)",
+    salary: 8000000,
+    currency: "JPY",
+    salaryPer: "YEAR",
+    expiresAt: dayjs().add(1, "month").toDate(),
+    employer: initialOrgYaedo.id,
+  },
+  {
+    title: "[WFH] Marketing lead",
+    description: `
+      We are hiring a marketing lead for our ebook store.
+
+      # Responsibilities
+
+      - Marketing for our ebook store
+      - Marketers management
+
+      # Requirements
+
+      ## Required
+
+      - Two year or more experience as a digital marketer in B2C field
+
+      ## Nice to have
+
+      - Basic understandings for HTML
+    `.trim(),
+    applicationUrl: "https://phanective.org/job-example-3",
+    location: "Remote (any location in Inazuma)",
+    salary: 8000000,
+    currency: "JPY",
+    salaryPer: "YEAR",
+    expiresAt: dayjs().add(1, "month").toDate(),
+    employer: initialOrgYaedo.id,
+  },
+  {
+    title: "Corporate Engineer",
+    description: `
+      We are hiring a software engineer for the Shogunate's internal systems.
+
+      # Responsibilities
+
+      - Development for the Shogunate's internal accounting systems (The Kanjou-kei)
+      - Vendor management for the Shogunate's internal accounting systems (The Kanjou-kei)
+
+      # Requirements
+
+      ## Required
+
+      - Two or more experience in Java
+
+      ## Nice to have
+
+      - Experience in vendor management
+    `.trim(),
+    applicationUrl: "https://phanective.org/job-example-4",
+    location: "The Hiiragi Estate, 4-1-1, Rito, Narukami Island, Inazuma",
+    salary: 9000000,
+    currency: "JPY",
+    salaryPer: "YEAR",
+    expiresAt: dayjs().add(1, "month").toDate(),
+    employer: initialOrgShogunate.id,
+  },
+  {
+    title: "Web designer",
+    description: `
+      We are hiring a web designer to promote Wangsheng Funeral Parlor.
+
+      # Responsibilities
+
+      - Create and maintain our websites
+
+      # Requirements
+
+      ## Required
+
+      - Two or more experience in web design
+      - Two or more experience in HTML and CSS
+
+      ## Nice to have
+
+      - Experience in JavaScript
+      - Experience in SEO
+      - Interest in the tradition of funerary practices in Liyue
+    `.trim(),
+    applicationUrl: "https://phanective.org/job-example-4",
+    location: "123456 Feiyun Slope, Liyue Harbor, Liyue",
+    salary: 4500000,
+    currency: "CNY",
+    salaryPer: "YEAR",
+    expiresAt: dayjs().add(1, "month").toDate(),
+    employer: initialOrgWangsheng.id,
+  },
+];
+
+const main = async () => {
+  if (process.env.SERVER_ENV !== "development" && process.env.SERVER_ENV !== "demo" && process.env.SERVER_ENV !== "demo-preview") {
+    console.info("Seeding are only allowed when SERVER_ENV is development, demo, or demo-preview. Seeding skipped.");
     return;
   }
 
-  const prisma = new PrismaClient({
-    adapter: new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
-    }),
-  });
-
-  const alreadySeeded = Boolean(await prisma.organization.findFirst())
-    || Boolean(await prisma.user.findFirst())
-    || Boolean(await prisma.job.findFirst());
+  const userExists = Boolean(await db.query.user.findFirst());
+  const jobExists = Boolean(await db.query.job.findFirst());
+  const alreadySeeded = userExists || jobExists;
 
   if (alreadySeeded) {
     console.info("The data already exists in the database. Skip seeding.");
     return;
   }
 
-  await prisma.organization.createMany({
-    data: [
-      {
-        id: "yph",
-        slug: "yaedo",
-        name: "Yae Publishing House, K.K.",
-        about: "Yae Publishing House is a leading light novel publisher from Inazuma. We also provide digital platforms to deliver the light novels and the other books to the global audience all over the Teyvat.",
-        url: "https://yaedo.example.co.jp",
-        addresses: [ "2-14-3, Hanamizaka, Inazuma City, Narukami Island, Inazuma" ],
-        emails: [ "hr@yaedo.example.co.jp" ],
-        currencies: [ "JPY" ],
-        createdAt: new Date(),
-        members: {
-          create: [
-            {
-              id: randomUUID(),
-              user: {
-                create: recruiterYae,
-              },
-              createdAt: new Date(),
-            },
-          ],
-        },
-      },
-      {
-        id: "kanjou",
-        slug: "kanjou",
-        name: "Kanjou Commission, The Shogunate of Inazuma",
-        about: "Kanjou Commission is a part of the Tri-Commission, The Shogunate of Inazuma. We are responsible for the Shogunate's financial and international affairs.",
-        url: "https://shogunate.example.go.jp",
-        addresses: [
-          "The Inazuma Castle, 1-1-1, Inazuma City, Narukami Island, Inazuma",
-          "Tenryou Commission Office, 1-2-5, Inazuma City, Narukami Island, Inazuma",
-          "The Hiiragi Estate, 5-1-1, Rito, Narukami Island, Inazuma",
-        ],
-        emails: [ "personnel@kanjou.example.go.jp" ],
-        currencies: [ "JPY" ],
-        createdAt: new Date(),
-        recruiters: [ recruiterRaiden, recruiterHiiragi ],
-      },
-      {
-        id: "wangsheng",
-        slug: "wangsheng",
-        name: "Wangsheng Funeral Parlor",
-        url: "https://wangsheng.example.com",
-        addresses: [
-          "123456 Feiyun Slope, Liyue Harbor, Liyue",
-        ],
-        emails: [ "hutao@wangsheng.example.com" ],
-        currencies: [ "CNY" ],
-        createdAt: new Date(),
-        recruiters: [ recruiterZhongli ],
-        // Lazy Hu Tao didn't write `about` ― to test layout when about is not written.
-      },
-    ],
+  // Allow N+1 problems for the readability
+  await db.transaction(async (tx) => {
+    await insertUsers(candidates, tx);
+
+    await insertOrganizations([
+      initialOrgYaedo,
+      initialOrgShogunate,
+      initialOrgWangsheng,
+    ], tx);
+
+    await tx.insert(job).values(initialJobs);
   });
 };
 
-await seed();
+await main();
--- src/lib/db/helpers.ts
+++ src/lib/db/helpers.ts
@@ -1,7 +1,40 @@
+import { randomUUID } from "node:crypto";
 import { eq, type InferInsertModel } from "drizzle-orm";
 import { db } from "./db.ts";
 import { userProps } from "./schema/user.ts";
-import { user as userTable } from "../../intermediate/better-auth-schema.ts"; // Can't use `@/` since this file isn't managed by Next.js
+import {
+  member,
+  organization as organizationTable,
+  user as userTable,
+} from "../../intermediate/better-auth-schema.ts"; // Can't use `@/` since this file isn't managed by Next.js
+
+type Transaction = Parameters<Parameters<typeof db.transaction>[0]>[0];
+
+export type UserWithProps = Omit<InferInsertModel<typeof userTable>, "propsId" | "recruitsFor"> & {
+  props: InferInsertModel<typeof userProps>;
+  recruitsFor?: string[];
+};
+
+export const insertUsers = async (users: UserWithProps | UserWithProps[], tx: Transaction) => {
+  const usersWithProps = Array.isArray(users) ? users : [ users ];
+  const newUserIds: string[] = [];
+
+  // TODO Fix N+1 problem
+  for (const userWithProps of usersWithProps) {
+    const { props, ...userWithoutProps } = userWithProps;
+
+    const [{ propsId }] = await tx.insert(userProps).values(props).returning({ propsId: userProps.id });
+
+    const [{ id: newUserId }] = await tx.insert(userTable).values({
+      ...userWithoutProps,
+      propsId: propsId,
+    }).returning({ id: userTable.id });
+
+    newUserIds.push(newUserId);
+  }
+
+  return newUserIds;
+};
 
 export const updateUserProps = (user: InferInsertModel<typeof userTable>) => {
   type UpdateFn = typeof db.update<typeof userProps>;
@@ -13,4 +46,32 @@ export const updateUserProps = (user: InferInsertModel<typeof userTable>) => {
       .set(...values)
       .where(eq(userProps.id, user.propsId)),
   };
+};
+
+export type OrgWithRecruiters = InferInsertModel<typeof organizationTable> & {
+  recruiters: UserWithProps[];
+};
+
+export const insertOrganizations = async (organizations: OrgWithRecruiters | OrgWithRecruiters[], tx: Transaction) => {
+  const orgsWithRecruiters = Array.isArray(organizations) ? organizations : [ organizations ];
+
+  // TODO Fix N+1 problem
+  for (const orgWithRecruiters of orgsWithRecruiters) {
+    const { recruiters = [], ...org } = orgWithRecruiters;
+
+    const [{ id: orgId }] = await tx.insert(organizationTable).values(org).returning({ id: organizationTable.id });
+
+    const newRecruiterIds = (0 < recruiters.length) ? await insertUsers(recruiters, tx) : [];
+
+    if (0 < newRecruiterIds.length) {
+      await tx.insert(member).values(
+        newRecruiterIds.map((newRecruiterId) => ({
+          organizationId: orgId,
+          userId: newRecruiterId,
+          id: randomUUID(),
+          createdAt: new Date(),
+        }))
+      );
+    }
+  }
 };
