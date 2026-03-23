c83f270ef729158f534d587b6716d7d732263341
Jumpei Ogawa <git@phanective.org>
2026-03-23 19:47:05 +0900

    refactor: migrate to Prisma ― types & validations

--- src/lib/validations/organization.ts
+++ src/lib/validations/organization.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import publicConfigs from "@/intermediate/public-configs.json";
-import { currency } from "@/lib/db/schema/currencies.ts";
+import { Currency } from "@/lib/prisma/enums.ts";
 import { mibToByte } from "@/lib/utils/utils.ts";
 
 export const orgNameSchema = z.string().trim().min(2, "Organization name must be at least 2 characters.");
@@ -32,7 +32,7 @@ export const orgAboutSchema = z.string().trim().optional();
 export const orgUrlSchema = z.url("Please enter a valid URL.");
 export const orgEmailSchema = z.email({ error: "Please enter a valid email here. Some legacy emails by Japanese mobile carriers may not be acceptable." });
 export const orgAddressSchema = z.string().trim().min(4, "Address must be at least 4 characters.");
-export const orgCurrencySchema = z.enum(currency.enumValues, "Please set available currency code. (e.g. \"USD\" for US Dollar)");
+export const orgCurrencySchema = z.enum(Currency, "Please set available currency code. (e.g. \"USD\" for US Dollar)");
 
 export const orgSchema = z.object({
   name: orgNameSchema,
@@ -42,7 +42,7 @@ export const orgSchema = z.object({
   url: orgUrlSchema,
   emails: z.array(orgEmailSchema).optional(),
   addresses: z.array(orgAddressSchema).min(1, "At least one address is required."),
-  currencies: z.array(z.enum(currency.enumValues)).min(1, "At least one currency is required."),
+  currencies: z.array(z.enum(Currency)).min(1, "At least one currency is required."),
 });
 
 export type Organization = z.infer<typeof orgSchema>;
