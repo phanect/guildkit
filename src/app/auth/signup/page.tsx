4bbfcbbfb73ae7620718d119d52ad91e8a6f9ad4
Jumpei Ogawa <git@phanective.org>
2026-03-24 20:12:50 +0900

    fix: allow sign up with Google

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,40 +1,82 @@
 "use client";
 
+import { useState, type ReactElement } from "react";
 import { Button, Link } from "@/components/generic/ButtonLink.tsx";
 import { signUpWith } from "@/lib/auth/client.ts";
-import type { ReactElement } from "react";
 
 export default function SignUpPage(): ReactElement {
+  const [ provider, setProvider ] = useState<"google" | "github" | undefined>(undefined);
+
   return (
     <>
       <h1 className="text-3xl font-bold mb-4">Create new user?</h1>
 
-      <p className="mb-5 text-center">
+      <p className="mb-8 text-center">
         Your account is not associated with GuildKit account.<br />
         Create new user or login with another account.
       </p>
 
-      <div className="flex flex-col items-center gap-2">
-        <Button
-          theme="button-deep"
-          className="w-64"
-          onClick={() => void signUpWith("github", "candidate")}
-        >
-          Create account as a <strong>candidate</strong>
-        </Button>
+      <h2 className="text-xl font-bold mb-4">
+        Choose account type to login
+      </h2>
 
-        <Button
-          theme="button-deep"
-          className="w-64"
-          onClick={() => void signUpWith("github", "recruiter")}
+      <div className="flex items-center gap-2 mb-6">
+        <input
+          type="radio"
+          id="provider-google"
+          name="provider"
+          value="google"
+          checked={provider === "google"}
+          onChange={(e) => e.target.checked && setProvider("google")}
+          className="sr-only peer/google"
+        />
+        <label
+          htmlFor="provider-google"
+          className="button-pale peer-checked/google:button-deep min-w-24"
         >
-          Create account as a <strong>recruiter</strong>
-        </Button>
+          Google
+        </label>
 
-        <Link href="/auth">
-          Login with another account
-        </Link>
+        <input
+          type="radio"
+          id="provider-github"
+          name="provider"
+          value="github"
+          checked={provider === "github"}
+          onChange={(e) => e.target.checked && setProvider("github")}
+          className="sr-only peer/github"
+        />
+        <label
+          htmlFor="provider-github"
+          className="button-pale peer-checked/github:button-deep min-w-24"
+        >
+          GitHub
+        </label>
       </div>
+
+      { provider && (
+        <div className="flex flex-col items-center gap-2">
+          <Button
+            theme="button-deep"
+            className="w-64"
+            onClick={() => void signUpWith(provider, "candidate")}
+          >
+            Create account as a <strong>candidate</strong>
+          </Button>
+
+          <Button
+            theme="button-deep"
+            className="w-64"
+            onClick={() => void signUpWith(provider, "recruiter")}
+          >
+            Create account as a <strong>recruiter</strong>
+          </Button>
+
+          <Link href="/auth">
+            Login with another account
+          </Link>
+        </div>
+      )}
     </>
   );
 }
